444a365c10bb397196b0c42e308c8d59b9158057
alexchen <alexchen@alexchen6265.online.com.tw>
2017-05-31 17:22:58 +0800

    update

--- AkbDmp/src/main/api/alex/test/PressureTestThreadWorker.java
+++ AkbDmp/src/main/api/alex/test/PressureTestThreadWorker.java
@@ -1,11 +1,22 @@
 package alex.test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.data.redis.connection.RedisClusterConfiguration;
 import org.springframework.data.redis.connection.RedisClusterConnection;
+import org.springframework.data.redis.connection.jedis.JedisClusterConnection;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.SetOperations;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import redis.clients.jedis.JedisPoolConfig;
 
 public class PressureTestThreadWorker implements Runnable {
 	Log log = LogFactory.getLog(PressureTestThreadWorker.class);
@@ -21,7 +32,11 @@ public class PressureTestThreadWorker implements Runnable {
 
 	@Override
 	public void run() {
-		SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
+//		SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
+		
+		
+		SetOperations<String, Object> opsForSet = getRedisTemplate().opsForSet();
+		
 		
 		log.info("redisTemplate:"+redisTemplate.opsForSet());
 //		String[] namePool = { "alex", "Nico", "bessie", "boris", "tim", "cool", "dyl", "park", "kylin", "hebe" };
@@ -53,4 +68,61 @@ public class PressureTestThreadWorker implements Runnable {
 			log.info(taskName+"_"+userName+">>>>>> finish ================= ");
 		}
 	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	 JedisPoolConfig jedisPoolConfig() {
+		 JedisPoolConfig jedisConfig = new JedisPoolConfig();
+//		 jedisConfig.setMaxIdle(maxIdle);
+//		 jedisConfig.setMaxWaitMillis(maxWait);
+//		 jedisConfig.setTestOnBorrow(testOnBorrow);
+		 jedisConfig.setMaxWaitMillis(-1);
+		 
+		//逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
+		 jedisConfig.setTimeBetweenEvictionRunsMillis(-1);
+		//在空闲时检查有效性, 默认false
+		 jedisConfig.setTestWhileIdle(false);
+		//设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
+		 jedisConfig.setEvictionPolicyClassName("org.apache.commons.pool2.impl.DefaultEvictionPolicy");
+		 return jedisConfig;
+	 }
+
+	public RedisClusterConfiguration getClusterConfiguration() {
+		Map<String, Object> source = new HashMap<String, Object>();
+		source.put("spring.redis.cluster.nodes", "192.168.2.207:6379,192.168.2.204:6379,192.168.2.205:6379,192.168.2.208:6379,192.168.2.209:6379,192.168.2.206:6379");
+		source.put("spring.redis.cluster.timeout", 5000);
+		source.put("spring.redis.cluster.max-redirects", 8);
+		return new RedisClusterConfiguration(new MapPropertySource("RedisClusterConfiguration", source));
+	}
+
+	public JedisConnectionFactory getConnectionFactory() {
+		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(getClusterConfiguration());
+		jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
+		return jedisConnectionFactory;
+	}
+
+	public JedisClusterConnection getJedisClusterConnection() {
+		return (JedisClusterConnection) getConnectionFactory().getConnection();
+	}
+
+	public RedisTemplate<String, Object> getRedisTemplate() {
+		RedisTemplate<String, Object> clusterTemplate = new RedisTemplate<String, Object>();
+		clusterTemplate.setConnectionFactory(getConnectionFactory());
+		clusterTemplate.setKeySerializer(new StringRedisSerializer());
+		clusterTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
+		return clusterTemplate;
+	}
+	
+	
+	
+	
+	
+	
 }
