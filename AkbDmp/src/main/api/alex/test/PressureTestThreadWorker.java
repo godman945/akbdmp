5fda4076aa58ab0ae93b9ff754404f2b17b0d1f3
alexchen <alexchen@alexchen6265.online.com.tw>
2017-05-31 17:29:30 +0800

    update

--- AkbDmp/src/main/api/alex/test/PressureTestThreadWorker.java
+++ AkbDmp/src/main/api/alex/test/PressureTestThreadWorker.java
@@ -33,8 +33,7 @@ public class PressureTestThreadWorker implements Runnable {
 	@Override
 	public void run() {
 		try{
-//			SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
-			SetOperations<String, Object> opsForSet = getRedisTemplate().opsForSet();
+			SetOperations<String, Object> opsForSet = redisTemplate.opsForSet();
 			log.info("redisTemplate:"+redisTemplate.opsForSet());
 //			String[] namePool = { "alex", "Nico", "bessie", "boris", "tim", "cool", "dyl", "park", "kylin", "hebe" };
 			String[] namePool = { "alex"};
@@ -69,61 +68,6 @@ public class PressureTestThreadWorker implements Runnable {
 		}
 
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	 JedisPoolConfig jedisPoolConfig() {
-		 JedisPoolConfig jedisConfig = new JedisPoolConfig();
-		 jedisConfig.setMaxIdle(200);
-		 jedisConfig.setMaxWaitMillis(300000);
-//		 jedisConfig.setTestOnBorrow(testOnBorrow);
-		 jedisConfig.setMaxWaitMillis(-1);
-		 
-		//逐出扫描的时间间隔(毫秒) 如果为负数,则不运行逐出线程, 默认-1
-		 jedisConfig.setTimeBetweenEvictionRunsMillis(-1);
-		//在空闲时检查有效性, 默认false
-		 jedisConfig.setTestWhileIdle(false);
-		//设置的逐出策略类名, 默认DefaultEvictionPolicy(当连接超过最大空闲时间,或连接数超过最大空闲连接数)
-		 jedisConfig.setEvictionPolicyClassName("org.apache.commons.pool2.impl.DefaultEvictionPolicy");
-		 return jedisConfig;
-	 }
-
-	public RedisClusterConfiguration getClusterConfiguration() {
-		Map<String, Object> source = new HashMap<String, Object>();
-		source.put("spring.redis.cluster.nodes", "192.168.2.207:6379,192.168.2.204:6379,192.168.2.205:6379,192.168.2.208:6379,192.168.2.209:6379,192.168.2.206:6379");
-		source.put("spring.redis.cluster.timeout", 5000);
-		source.put("spring.redis.cluster.max-redirects", 8);
-		return new RedisClusterConfiguration(new MapPropertySource("RedisClusterConfiguration", source));
-	}
-
-	public JedisConnectionFactory getConnectionFactory() {
-		JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory(getClusterConfiguration());
-		jedisConnectionFactory.setPoolConfig(jedisPoolConfig());
-		return jedisConnectionFactory;
-	}
-
-	public JedisClusterConnection getJedisClusterConnection() {
-		return (JedisClusterConnection) getConnectionFactory().getConnection();
-	}
-
-	public RedisTemplate<String, Object> getRedisTemplate() {
-		RedisTemplate<String, Object> clusterTemplate = new RedisTemplate<String, Object>();
-		clusterTemplate.setConnectionFactory(getConnectionFactory());
-		clusterTemplate.setKeySerializer(new StringRedisSerializer());
-		clusterTemplate.setDefaultSerializer(new GenericJackson2JsonRedisSerializer());
-		return clusterTemplate;
-	}
-	
-	
-	
-	
 	
 	
 }
