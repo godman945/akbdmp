eae7eece920dcda4adb47fc6d7d9fedce33244a3
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-02 18:39:33 +0800

    update

--- AkbDmp/src/main/api/com/pchome/akbdmp/spring/config/bean/aop/AopConfig.java
+++
@@ -1,8 +0,0 @@
-package com.pchome.akbdmp.spring.config.bean.aop;
-
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-
-@EnableAspectJAutoProxy(proxyTargetClass = true)
-public class AopConfig {
-
-}
