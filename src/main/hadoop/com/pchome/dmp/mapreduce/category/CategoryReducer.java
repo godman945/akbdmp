c396c39706b451cd030f9c132331abb412ba25b5
bessie <bessie@bessie6166.online.com.tw>
2017-06-07 10:01:21 +0800

    update

--- src/main/hadoop/com/pchome/dmp/mapreduce/category/CategoryReducer.java
+++ src/main/hadoop/com/pchome/dmp/mapreduce/category/CategoryReducer.java
@@ -128,54 +128,52 @@ public class CategoryReducer extends Reducer<Text, Text, Text, Text> {
 			for (EnumCategoryJob enumCategoryJob : EnumCategoryJob.values()) { // Category_Count_Ck,Category_Count_Pv
 				AncestorJob job = FactoryCategoryJob.getInstance(enumCategoryJob);
 				if (enumCategoryJob.getClassName().equals(EnumCategoryJob.Category_Count_Ck.getClassName())) {
-					sendKafkaJson.put("type", "mongo");
-					sendKafkaJson.put("action", "insert");
-					sendKafkaJson.put("collection", "class_count");
-					sendKafkaJson.put("column", "");
-					sendKafkaJson.put("condition", "");
-					sendKafkaJson.put("Test", "ck ck ck");
-					sendKafkaJson.put("content", job.list.toString());
-					kafka.add(sendKafkaJson);
-					kafkaUtil.sendMessage("TEST", "", kafka.toString());
-//					for (DBObject jsonObject : job.list) {
-//						sendKafkaJson.put("type", "mongo");
-//						sendKafkaJson.put("action", "insert");
-//						sendKafkaJson.put("collection", "class_count");
-//						sendKafkaJson.put("column", "");
-//						sendKafkaJson.put("condition", "");
-//						sendKafkaJson.put("Test", "ck ck ck");
-//						sendKafkaJson.put("content", jsonObject.toString());
-//						kafka.add(sendKafkaJson);
-//						kafkaUtil.sendMessage("TEST", "", kafka.toString());
-//					}
-					
+//					sendKafkaJson.put("type", "mongo");
+//					sendKafkaJson.put("action", "insert");
+//					sendKafkaJson.put("collection", "class_count");
+//					sendKafkaJson.put("column", "");
+//					sendKafkaJson.put("condition", "");
+//					sendKafkaJson.put("Test", "ck ck ck");
+//					sendKafkaJson.put("content", job.list.toString());
+//					kafka.add(sendKafkaJson);
+//					kafkaUtil.sendMessage("TEST", "", kafka.toString());
+					for (DBObject jsonObject : job.list) {
+						sendKafkaJson.put("type", "mongo");
+						sendKafkaJson.put("action", "insert");
+						sendKafkaJson.put("collection", "class_count");
+						sendKafkaJson.put("column", "");
+						sendKafkaJson.put("condition", "");
+						sendKafkaJson.put("Test", "ck ck ck");
+						sendKafkaJson.put("content", jsonObject.toString());
+						kafka.add(sendKafkaJson);
+						kafkaUtil.sendMessage("TEST", "", kafka.toString());
+					}
 				}
 				
 				if (enumCategoryJob.getClassName().equals(EnumCategoryJob.Category_Count_Pv.getClassName())) {
-					sendKafkaJson.put("type", "mongo");
-					sendKafkaJson.put("action", "insert");
-					sendKafkaJson.put("collection", "class_count");
-					sendKafkaJson.put("column", "");
-					sendKafkaJson.put("condition", "");
-					sendKafkaJson.put("Test", "PV pV pV");
-					sendKafkaJson.put("content", job.list.toString());
-					kafka.add(sendKafkaJson);
-					kafkaUtil.sendMessage("TEST", "", kafka.toString());
-//					for (DBObject jsonObject : job.list) {
-//						sendKafkaJson.put("type", "mongo");
-//						sendKafkaJson.put("action", "insert");
-//						sendKafkaJson.put("collection", "class_count");
-//						sendKafkaJson.put("column", "");
-//						sendKafkaJson.put("condition", "");
-//						sendKafkaJson.put("Test", "PV PV PV");
-//						sendKafkaJson.put("content", jsonObject.toString());
-//						kafka.add(sendKafkaJson);
-//						kafkaUtil.sendMessage("TEST", "", kafka.toString());
-//					}
+//					sendKafkaJson.put("type", "mongo");
+//					sendKafkaJson.put("action", "insert");
+//					sendKafkaJson.put("collection", "class_count");
+//					sendKafkaJson.put("column", "");
+//					sendKafkaJson.put("condition", "");
+//					sendKafkaJson.put("Test", "PV pV pV");
+//					sendKafkaJson.put("content", job.list.toString());
+//					kafka.add(sendKafkaJson);
+//					kafkaUtil.sendMessage("TEST", "", kafka.toString());
+					for (DBObject jsonObject : job.list) {
+						sendKafkaJson.put("type", "mongo");
+						sendKafkaJson.put("action", "insert");
+						sendKafkaJson.put("collection", "class_count");
+						sendKafkaJson.put("column", "");
+						sendKafkaJson.put("condition", "");
+						sendKafkaJson.put("Test", "PV PV PV");
+						sendKafkaJson.put("content", jsonObject.toString());
+						kafka.add(sendKafkaJson);
+						kafkaUtil.sendMessage("TEST", "", kafka.toString());
+					}
 				}
 			}
-			kafkaUtil.sendMessage("TEST", "", "ck ck ");
-			kafkaUtil.sendMessage("TEST", "","PV pV");
+			kafkaUtil.sendMessage("TEST", "", "20170607 test ok ");
 			kafkaUtil.close();
 			//bessie-end
 			
