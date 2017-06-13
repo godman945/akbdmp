ba5811c2483c4410ba4c81335dec7c0408585c3f
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-13 18:26:20 +0800

    update

--- src/main/hadoop/com/pchome/dmp/mapreduce/job/categorylog/CategoryLogMapper.java
+++ src/main/hadoop/com/pchome/dmp/mapreduce/job/categorylog/CategoryLogMapper.java
@@ -56,42 +56,42 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 		Configuration conf = context.getConfiguration();
 		try {
 			System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
-//			org.apache.hadoop.fs.Path[] path = DistributedCache.getLocalCacheFiles(conf);
-//			Path clsfyTable = Paths.get(path[1].toString());
-//			Charset charset = Charset.forName("UTF-8");
-//			List<String> lines = Files.readAllLines(clsfyTable, charset);
-//			for (String line : lines) {
-//				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
-//				String[] tmpStrAry2 = tmpStrAry[1].split(",");
-//				clsfyCraspMap.put(tmpStrAry[0],	new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
-//			}
+			org.apache.hadoop.fs.Path[] path = DistributedCache.getLocalCacheFiles(conf);
+			Path clsfyTable = Paths.get(path[1].toString());
+			Charset charset = Charset.forName("UTF-8");
+			List<String> lines = Files.readAllLines(clsfyTable, charset);
+			for (String line : lines) {
+				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
+				String[] tmpStrAry2 = tmpStrAry[1].split(",");
+				clsfyCraspMap.put(tmpStrAry[0],	new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
+			}
 			
 			
 			// get csv file
-//			Path cate_path = Paths.get(path[0].toString());
-//			charset = Charset.forName("UTF-8");
-//
-//			int maxCateLvl = 4;
-//			list = new ArrayList<Map<String, String>>();
-//
-//			for (int i = 0; i < maxCateLvl; i++) {
-//				list.add(new HashMap<String, String>());
-//			}
-//			
-//			lines.clear();
-//			lines = Files.readAllLines(cate_path, charset);
+			Path cate_path = Paths.get(path[0].toString());
+			charset = Charset.forName("UTF-8");
+
+			int maxCateLvl = 4;
+			list = new ArrayList<Map<String, String>>();
+
+			for (int i = 0; i < maxCateLvl; i++) {
+				list.add(new HashMap<String, String>());
+			}
+			
+			lines.clear();
+			lines = Files.readAllLines(cate_path, charset);
 
 			// 將 table: pfp_ad_category_new 內容放入list中(共有 maxCateLvl 層)
-//			for (String line : lines) {
-//				String[] tmpStr = line.split(";");
-//
-//				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
-//				if (lvl <= maxCateLvl) {
-//					list.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),
-//							tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
-//				}
-//
-//			}
+			for (String line : lines) {
+				String[] tmpStr = line.split(";");
+
+				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
+				if (lvl <= maxCateLvl) {
+					list.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),
+							tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
+				}
+
+			}
 		} catch (Exception e) {
 			// log.error("ClsfyGndAgeCrspTable error:\n" + e.getMessage());
 			// e.printStackTrace();
@@ -103,16 +103,16 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 	public void map(LongWritable offset, Text value, Context context) {
 		// log.info("value=" + value);
 
-//		if (StringUtils.isBlank(value.toString())) {
-//			// log.info("value is blank");
-//			return;
-//		}
-//
-//		String[] values = value.toString().split(SYMBOL);
-//		if (values.length < LOG_LENGTH) {
-//			log.info("values.length < " + LOG_LENGTH);
-//			return;
-//		}
+		if (StringUtils.isBlank(value.toString())) {
+			// log.info("value is blank");
+			return;
+		}
+
+		String[] values = value.toString().split(SYMBOL);
+		if (values.length < LOG_LENGTH) {
+			log.info("values.length < " + LOG_LENGTH);
+			return;
+		}
 
 		AncestorJob job = null;
 		String key = null;
@@ -124,7 +124,7 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 		// values[13] //ck,pv
 		// values[4] //url
 		// values[15] //ad_class
-//		CategoryLogBean result = null;
+		CategoryLogBean result = null;
 		try {
 			// 1.reg待補
 			// 2.走ad_click
@@ -134,12 +134,12 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 //			}
 
 			// 露天
-//			if (values[13].equals("pv") && StringUtils.isNotBlank(values[4]) && values[4].matches("reten")) {
-//				ACategoryLogData aCategoryLogData = categoryLogFactory.getACategoryLogObj(CategoryLogEnum.PV_RETUN);
-//				categoryLogBean.setClsfyCraspMap(clsfyCraspMap);
-//				categoryLogBean.setList(list);
-//				result = (CategoryLogBean)aCategoryLogData.processCategory(values, personalInfoFactory, categoryLogBean);
-//			}
+			if (values[13].equals("pv") && StringUtils.isNotBlank(values[4]) && values[4].matches("reten")) {
+				ACategoryLogData aCategoryLogData = categoryLogFactory.getACategoryLogObj(CategoryLogEnum.PV_RETUN);
+				categoryLogBean.setClsfyCraspMap(clsfyCraspMap);
+				categoryLogBean.setList(list);
+				result = (CategoryLogBean)aCategoryLogData.processCategory(values, personalInfoFactory, categoryLogBean);
+			}
 
 			// 24h
 //			if (values[13].equals("pv") && StringUtils.isNotBlank(values[4]) && values[4].matches("24h")) {
@@ -147,8 +147,8 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 //				aCategoryLogData.processCategory(values, personalInfoFactory, categoryLogBean);
 //			}
 			
-//			String result2 = result.getMemid() +SYMBOL+result.getUuid()+SYMBOL+result.getAdClass()+SYMBOL+result.getAge()+SYMBOL+result.getSex();
-			String result2 = "ssssss";
+			String result2 = result.getMemid() +SYMBOL+result.getUuid()+SYMBOL+result.getAdClass()+SYMBOL+result.getAge()+SYMBOL+result.getSex();
+//			String result2 = "ssssss";
 			keyOut.set(key);
 			valueOut.set(result2);
 			context.write(keyOut, valueOut);
