2475003383a42c6c7fc483a2fbe7b10489c40042
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-14 15:31:42 +0800

    update

--- src/main/hadoop/com/pchome/dmp/mapreduce/job/categorylog/CategoryLogMapper.java
+++ src/main/hadoop/com/pchome/dmp/mapreduce/job/categorylog/CategoryLogMapper.java
@@ -92,67 +92,49 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 
 	@Override
 	public void map(LongWritable offset, Text value, Context context) {
-		Path cate_path = Paths.get("D:/home/webuser/pfp_ad_category_new.csv");
-		Path clsfyTable = Paths.get("D:/home/webuser/ClsfyGndAgeCrspTable.txt");
-		Charset charset = Charset.forName("UTF-8");
+//		Path cate_path = Paths.get("D:/home/webuser/pfp_ad_category_new.csv");
+//		Path clsfyTable = Paths.get("D:/home/webuser/ClsfyGndAgeCrspTable.txt");
+//		Charset charset = Charset.forName("UTF-8");
+//		try {
+//			List<String> lines = Files.readAllLines(clsfyTable, charset);
+//			for (String line : lines) {
+//				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
+//				String[] tmpStrAry2 = tmpStrAry[1].split(",");
+//				clsfyCraspMap.put(tmpStrAry[0],	new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
+//			}
+//			
+//			// get csv file
+//
+//			int maxCateLvl = 4;
+//			list = new ArrayList<Map<String, String>>();
+//
+//			for (int i = 0; i < maxCateLvl; i++) {
+//				list.add(new HashMap<String, String>());
+//			}
+//			
+//			lines.clear();
+//			lines = Files.readAllLines(cate_path, charset);
+//
+//			// 將 table: pfp_ad_category_new 內容放入list中(共有 maxCateLvl 層)
+//			for (String line : lines) {
+//				String[] tmpStr = line.split(";");
+//				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
+//				if (lvl <= maxCateLvl) {
+//					list.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
+//				}
+//
+//			}
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
 		
-		try {
-			
-			List<String> lines = Files.readAllLines(clsfyTable, charset);
-			for (String line : lines) {
-				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
-				String[] tmpStrAry2 = tmpStrAry[1].split(",");
-				clsfyCraspMap.put(tmpStrAry[0],	new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
-			}
-			
-			// get csv file
-
-			int maxCateLvl = 4;
-			list = new ArrayList<Map<String, String>>();
-
-			for (int i = 0; i < maxCateLvl; i++) {
-				list.add(new HashMap<String, String>());
-			}
-			
-			lines.clear();
-			lines = Files.readAllLines(cate_path, charset);
-
-			// 將 table: pfp_ad_category_new 內容放入list中(共有 maxCateLvl 層)
-			for (String line : lines) {
-				String[] tmpStr = line.split(";");
-				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
-				if (lvl <= maxCateLvl) {
-					list.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
-				}
-
-			}
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		
+		String[] values = value.toString().split(SYMBOL);
+		if (values.length < LOG_LENGTH) {
+			log.info("values.length < " + LOG_LENGTH);
+			return;
 		}
-		
-		
-		
-		
-		
-		
-		
-		// log.info("value=" + value);
-
-//		if (StringUtils.isBlank(value.toString())) {
-//			// log.info("value is blank");
-//			return;
-//		}
-//
-//		String[] values = value.toString().split(SYMBOL);
-//		if (values.length < LOG_LENGTH) {
-//			log.info("values.length < " + LOG_LENGTH);
-//			return;
-//		}
-//
-//		AncestorJob job = null;
-		String key = null;
-		String val = null;
 
 		// 1.
 		// values[1] //mid
@@ -162,13 +144,13 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 		// values[15] //ad_class
 		// values[3] //behavior
 		
-		String [] values =  new String[100];
-		values[1] = "sts0330";
-		values[2] = "alex2";
-		values[13] = "pv";
-		values[4] = "http://24h.pchome.com.tw/region/DICM";
-		values[15] = "alex5";
-		values[3] = "alex6";
+//		String [] values =  new String[100];
+//		values[1] = "sts0330";
+//		values[2] = "alex2";
+//		values[13] = "pv";
+//		values[4] = "http://24h.pchome.com.tw/region/DICM";
+//		values[15] = "alex5";
+//		values[3] = "alex6";
 		
  		log.info(">>>>>> "+values[1]);
 		log.info(">>>>>> "+values[2]);
