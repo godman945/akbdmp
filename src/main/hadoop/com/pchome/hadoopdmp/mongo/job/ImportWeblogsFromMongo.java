62aac15cbade68600fba3dfd16e67c9a7ad528bc
bessie <bessie@bessie6166.online.com.tw>
2017-06-22 11:24:08 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/ImportWeblogsFromMongo.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/ImportWeblogsFromMongo.java
@@ -4,19 +4,16 @@ import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.bson.BSONObject;
 
 import com.mongodb.hadoop.MongoInputFormat;
 import com.mongodb.hadoop.util.MongoConfigUtil;
-import com.pchome.hadoopdmp.mongo.job.MongoIn.MyReducer;
 
 public class ImportWeblogsFromMongo {
 
