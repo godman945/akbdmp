bcd89a7697f28467e48ab1398f117f9a237ab8cb
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-23 14:38:24 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
@@ -26,66 +26,71 @@ private static Log log = LogFactory.getLog("MapReduceMongoJob");
 	
 	public static class ReadWeblogsFromMongo extends Mapper<Object, BSONObject, Text, Text> {
 		public void map(Object key, BSONObject value, Context context) throws IOException, InterruptedException {
-			// System.out.println("Key: " + key);
-			// System.out.println("Value: " + value);
-			// String uuid = value.get("uuid").toString();
-			// String behavior = value.get("behavior").toString();
-			// String record_date = value.get("record_date").toString();
-			
-			String user_id = value.get("user_id").toString();
-			
-			
-//			String type = value.get("type").toString();
-			String update_date = value.get("update_date").toString();
-			String category_info_str = value.get("category_info").toString();
-			
-			Gson gson = new Gson();
-			TypeToken<List<Map<String, Object>>> token = new TypeToken<List<Map<String, Object>>>(){};
-			List<Map<String, Object>> personList = gson.fromJson(category_info_str, token.getType());
-			
-			log.info(">>>>>> mapper user_id : "+user_id);
-//			log.info(">>>>>> mapper type : "+type);
-			log.info(">>>>>> mapper update_date : "+update_date);
-			log.info(">>>>>> mapper category_info_str : "+category_info_str);
-			
-			
-			
-			
-			
-			String category = "";
-//			String user_adclass = user_id+"_"+type;
-			for (Map<String, Object> map : personList) {
-				category = (String) map.get("category");
-//				log.info(">>>>>> mapper category : "+category);	
+			try{
+				// System.out.println("Key: " + key);
+				// System.out.println("Value: " + value);
+				// String uuid = value.get("uuid").toString();
+				// String behavior = value.get("behavior").toString();
+				// String record_date = value.get("record_date").toString();
 				
-//				String adclass_user =  type + "_" + category;
-//				
-//				context.write(new Text(adclass_user), new Text("1"));
+				String user_id = value.get("user_id").toString();
+				
+				
+				String type = value.get("type").toString();
+				String update_date = value.get("update_date").toString();
+				String category_info_str = value.get("category_info").toString();
+				
+				Gson gson = new Gson();
+				TypeToken<List<Map<String, Object>>> token = new TypeToken<List<Map<String, Object>>>(){};
+				List<Map<String, Object>> personList = gson.fromJson(category_info_str, token.getType());
+				
+				log.info(">>>>>> mapper user_id : "+user_id);
+				log.info(">>>>>> mapper type : "+type);
+				log.info(">>>>>> mapper update_date : "+update_date);
+				log.info(">>>>>> mapper category_info_str : "+category_info_str);
+				
+				
+				
+				
+				
+				String category = "";
+//				String user_adclass = user_id+"_"+type;
+				for (Map<String, Object> map : personList) {
+					category = (String) map.get("category");
+//					log.info(">>>>>> mapper category : "+category);	
+					
+//					String adclass_user =  type + "_" + category;
+//					
+//					context.write(new Text(adclass_user), new Text("1"));
+				}
+				
+				context.write(new Text("TEST"), new Text("1"));
+				
+//				log.info(">>>>> mapper category_info_str : "+category);
+////				context.write(new Text(user_id), new Text(update_date));
+//				context.write(new Text("total_user"), new Text("1"));
+				
+				
+//				ObjectMapper mapper = new ObjectMapper();
+//				ReadMongoBean user = mapper.readValue(category_info_str, ReadMongoBean.class);
+//				System.out.println("user Bean: "+user.getCategory_info().size());
+//					
+//				String category="";
+//				for (Map<String, Object> map : user.getCategory_info()) {
+//					category=(String) map.get("category");
+//					System.out.println("category : "+category);
+//					
+//				}
+				
+				
+				// String date = value.get("date").toString();
+				// String time = value.get("time").toString();
+				// String ip = value.get("ip").toString();
+				// String output = behavior+" "+record_date;
+			}catch(Exception e){
+				log.error(">>>>> mapper e : "+e.getMessage());
 			}
-			
-			context.write(new Text("TEST"), new Text("1"));
-			
-//			log.info(">>>>> mapper category_info_str : "+category);
-////			context.write(new Text(user_id), new Text(update_date));
-//			context.write(new Text("total_user"), new Text("1"));
-			
-			
-//			ObjectMapper mapper = new ObjectMapper();
-//			ReadMongoBean user = mapper.readValue(category_info_str, ReadMongoBean.class);
-//			System.out.println("user Bean: "+user.getCategory_info().size());
-//				
-//			String category="";
-//			for (Map<String, Object> map : user.getCategory_info()) {
-//				category=(String) map.get("category");
-//				System.out.println("category : "+category);
-//				
-//			}
-			
-			
-			// String date = value.get("date").toString();
-			// String time = value.get("time").toString();
-			// String ip = value.get("ip").toString();
-			// String output = behavior+" "+record_date;
+	
 
 			
 		}
