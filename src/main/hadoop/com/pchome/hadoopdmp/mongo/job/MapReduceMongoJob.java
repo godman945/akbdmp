b46ca54b0adce21547df40e270f2b909345642c8
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-23 17:27:56 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
@@ -19,83 +19,85 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.bson.BSONObject;
 
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 import com.mongodb.hadoop.MongoInputFormat;
 import com.mongodb.hadoop.util.MongoConfigUtil;
 
 public class MapReduceMongoJob {
-private static Log log = LogFactory.getLog("MapReduceMongoJob");
-	
+	private static Log log = LogFactory.getLog("MapReduceMongoJob");
+
 	public static class ReadWeblogsFromMongo extends Mapper<Object, BSONObject, Text, Text> {
 		public void map(Object key, BSONObject value, Context context) throws IOException, InterruptedException {
-			try{
+			try {
 				String update_date = value.get("update_date").toString();
 				String category_info_str = value.get("category_info").toString();
 				String user_id = value.get("user_id").toString();
 				Map<String, Object> user_info = (Map<String, Object>) value.get("user_info");
-				List<Map<String, Object>> category_info =  (List<Map<String, Object>>) value.get("category_info");
-				String userType= user_info.get("type").toString();
-				Map<String,Set<String>> allMap = new HashMap<String, Set<String>>();
-				
-//				0015022500000000
-//				0015022720350000
+				List<Map<String, Object>> category_info = (List<Map<String, Object>>) value.get("category_info");
+				String userType = user_info.get("type").toString();
+				Map<String, Set<String>> allMap = new HashMap<String, Set<String>>();
+
+				// 0015022500000000
+				// 0015022720350000
 				String group = "bessie";
 				for (Map<String, Object> category : category_info) {
 					String ad_class = category.get("category").toString();
-					String categoryKey = ad_class+"_"+userType.toUpperCase();
-					log.info(">>>>>> categoryKey:"+categoryKey);
-					
-					if(!allMap.containsKey(ad_class)){
+					String categoryKey = ad_class + "_" + userType.toUpperCase();
+					log.info(">>>>>> categoryKey:" + categoryKey);
+
+					if (!allMap.containsKey(ad_class)) {
 						Set<String> set = new HashSet<>();
 						set.add(user_id);
 						allMap.put(ad_class, set);
-					}else{
+					} else {
 						Set<String> set = allMap.get(ad_class);
 						set.add(user_id);
 					}
 					context.write(new Text(categoryKey), new Text());
 				}
-				
+
 				Set<String> data = new HashSet<>();
 				for (Map.Entry<String, Set<String>> entry : allMap.entrySet()) {
-					if(entry.equals("0015022500000000")){
+					if (entry.equals("0015022500000000")) {
 						data.addAll(entry.getValue());
 					}
-					if(entry.equals("0015022720350000")){
+					if (entry.equals("0015022720350000")) {
 						data.addAll(entry.getValue());
 					}
 				}
-				context.write(new Text("0015022500000000_0015022720350000_TOTAL"), new Text(String.valueOf(data.size())));
-				
-				
-			}catch(Exception e){
-				log.error(">>>>> mapper e : "+e.getMessage());
+				context.write(new Text("0015022500000000_0015022720350000_TOTAL"),
+						new Text(String.valueOf(data.size())));
+
+			} catch (Exception e) {
+				log.error(">>>>> mapper e : " + e.getMessage());
 			}
 		}
 	}
 
 	public static class MyReducer extends Reducer<Text, Text, Text, Text> {
 		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
-			log.info(">>>>> reduce key: "+key);
-			log.info(">>>>> reduce values: "+values);
-			int sum = 0;
-			
-			if(key.toString().indexOf("TOTAL") > 0){
-				log.info(">>>>> reduce TEST: "+key);
-				log.info(">>>>> reduce TEST: "+values);
-				for (Text text : values) {
-					log.info(">>>>> reduce alex TEST: "+values);
-					context.write(key,new Text(text));
+			try {
+				log.info(">>>>> reduce key: " + key);
+				log.info(">>>>> reduce values: " + values);
+				int sum = 0;
+
+				if (key.toString().indexOf("TOTAL") > 0) {
+					log.info(">>>>> reduce TEST: " + key);
+					log.info(">>>>> reduce TEST: " + values);
+					for (Text text : values) {
+						log.info(">>>>> reduce alex TEST: " + values);
+						context.write(key, new Text(text));
+					}
+
+				} else {
+					for (Text text : values) {
+						sum = sum + 1;
+						log.info(">>>>> reduce sum: " + sum);
+
+					}
+					context.write(key, new Text(String.valueOf(sum)));
 				}
-				
-			}else{
-				for (Text text : values) {
-					sum = sum + 1;
-					log.info(">>>>> reduce sum: "+sum);
-					
-				}
-				context.write(key,new Text(String.valueOf(sum)));
+			} catch (Exception e) {
+				log.error(">>>>> mapper e : " + e.getMessage());
 			}
 		}
 	}
@@ -103,10 +105,12 @@ private static Log log = LogFactory.getLog("MapReduceMongoJob");
 	public static void main(String[] args) throws Exception {
 		final Configuration conf = new Configuration();
 		MongoConfigUtil.setInputURI(conf, "mongodb://192.168.1.37:27017/pcbappdev.class_count");
-//		conf.set("mongo.input.query",  "{'update_date':{'$gt':{'$date':'2017-06-01 23:59:59'}}}");
-//		conf.set("mongo.input.query",  "{'update_date':{'$gt':'2017-06-19 23:59:59'}}");
+		// conf.set("mongo.input.query",
+		// "{'update_date':{'$gt':{'$date':'2017-06-01 23:59:59'}}}");
+		// conf.set("mongo.input.query", "{'update_date':{'$gt':'2017-06-19
+		// 23:59:59'}}");
 		MongoConfigUtil.setCreateInputSplits(conf, false);
-		
+
 		System.out.println("Configuration: " + conf);
 		final Job job = new Job(conf, "alex_test");
 		Path out = new Path("/home/webuser/alex/mongo");
