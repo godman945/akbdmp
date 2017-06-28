3a795e7761180a2d203392fbd0a6e1d4b6cad40a
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-28 10:27:55 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
@@ -88,10 +88,10 @@ public class MapReduceMongoJob {
 
 					//process parent
 					for (Entry<String, String> entry : categoryMap.entrySet()) {
-						log.info(">>>>>> entry_key:"+entry.getKey());
-						log.info(">>>>>> entry_value:"+entry.getValue());
-						
+						log.info(">>>>>> entry_key:" + entry.getKey());
+						log.info(">>>>>> entry_value:" + entry.getValue());
 						if(entry.getKey().contains(ad_class)){
+							log.info(">>>>>> TRUE:" +entry.getValue()+":"+ user_id);
 							context.write(new Text(entry.getValue()), new Text(user_id));
 						}
 					}
