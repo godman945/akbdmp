410241530a37404b00928bcefb540ca9b5294dce
alexchen <alexchen@alexchen6265.online.com.tw>
2017-06-28 12:05:39 +0800

    update

---
+++ src/main/hadoop/com/pchome/hadoopdmp/mysql/db/dao/ad/IAdmAdGroupDao.java
@@ -0,0 +1,19 @@
+package com.pchome.hadoopdmp.mysql.db.dao.ad;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.pchome.hadoopdmp.data.mysql.pojo.AdmAdGroup;
+import com.pchome.hadoopdmp.mysql.db.dao.base.IBaseDAO;
+
+@Repository
+public interface IAdmAdGroupDao extends IBaseDAO<AdmAdGroup, Integer>{
+
+//	public List<Object> getPcsUserByKeyword(String pcsUserId,String keyword) throws Exception;
+//
+//	public boolean checkPcsUserId(String pcsUserId) throws Exception;
+//	
+//	public PcsUser getPcsUserByFbid(String fbid) throws Exception;
+//	
+//	public List<PcsUser> getPcsUserByFbidList(String fbid) throws Exception;
+}
