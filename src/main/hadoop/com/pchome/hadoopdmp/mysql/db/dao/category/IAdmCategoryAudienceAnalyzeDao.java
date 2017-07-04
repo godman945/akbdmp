b7223453bdb262931aaa139a5808aa3438d66971
bessie <bessie@bessie6166.online.com.tw>
2017-07-04 10:18:51 +0800

    update

---
+++ src/main/hadoop/com/pchome/hadoopdmp/mysql/db/dao/category/IAdmCategoryAudienceAnalyzeDao.java
@@ -0,0 +1,12 @@
+package com.pchome.hadoopdmp.mysql.db.dao.category;
+
+
+import org.springframework.stereotype.Repository;
+
+import com.pchome.hadoopdmp.data.mysql.pojo.AdmCategoryAudienceAnalyze;
+import com.pchome.hadoopdmp.mysql.db.dao.base.IBaseDAO;
+
+@Repository
+public interface IAdmCategoryAudienceAnalyzeDao extends IBaseDAO<AdmCategoryAudienceAnalyze, Integer>{
+
+}
