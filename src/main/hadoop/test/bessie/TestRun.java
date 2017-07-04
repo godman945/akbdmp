ac581343786e0785cbc52b2ac3d704ab834be13b
bessie <bessie@bessie6166.online.com.tw>
2017-07-04 18:04:20 +0800

    update

--- src/main/hadoop/test/bessie/TestRun.java
+++ src/main/hadoop/test/bessie/TestRun.java
@@ -1,13 +1,10 @@
 package test.bessie;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,13 +13,8 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import org.springframework.stereotype.Component;
 
 import com.pchome.hadoopdmp.data.mysql.pojo.AdmCategory;
-import com.pchome.hadoopdmp.data.mysql.pojo.AdmCategoryAnalyze;
 import com.pchome.hadoopdmp.data.mysql.pojo.AdmCategoryGroup;
-import com.pchome.hadoopdmp.data.mysql.pojo.AdmCategoryGroupAnalyze;
 import com.pchome.hadoopdmp.mysql.db.service.category.IAdmCategoryService;
-import com.pchome.hadoopdmp.mysql.db.service.categoryanalyze.AdmCategoryGroupAnalyzeService;
-import com.pchome.hadoopdmp.mysql.db.service.categoryanalyze.IAdmCategoryAnalyzeService;
-import com.pchome.hadoopdmp.mysql.db.service.categoryanalyze.IAdmCategoryGroupAnalyzeService;
 import com.pchome.hadoopdmp.mysql.db.service.categorygroup.IAdmCategoryGroupService;
 import com.pchome.hadoopdmp.spring.config.bean.allbeanscan.SpringAllHadoopConfig;
 import com.pchome.soft.util.DateFormatUtil;
@@ -41,11 +33,6 @@ public class TestRun {
 	@Autowired
 	private IAdmCategoryService admCategoryService;
 	
-	@Autowired
-	private IAdmCategoryGroupAnalyzeService admGroupAnalyzeService;
-	
-	@Autowired
-	private IAdmCategoryAnalyzeService admCategoryAnalyzeService;
 	
 	
 //	//查詢
@@ -98,47 +85,6 @@ public class TestRun {
 		admCategoryService.save(admCategory);
 	}
 	
-	//新增adm_category_group_analyze
-	private void hibernateDbTest4() throws Exception {
-		Date date = new Date();
-		String dateStr = dateFormatUtil.getDateTemplate2().format(date);
-		date = dateFormatUtil.getDateTemplate2().parse(dateStr);
-
-		AdmCategoryGroupAnalyze admCategoryGroupAnalyze = new AdmCategoryGroupAnalyze();
-		admCategoryGroupAnalyze.setAdClassCountByHistory(333);
-		admCategoryGroupAnalyze.setAdGroupId("0000000000000006");
-		admCategoryGroupAnalyze.setUserIdType("");
-		admCategoryGroupAnalyze.setCreateDate(new Date());
-		admGroupAnalyzeService.save(admCategoryGroupAnalyze);
-
-	}
-	
-	//新增adm_category_group_analyze
-	private void hibernateDbTest5() throws Exception {
-		AdmCategoryAnalyze admCategoryAnalyze = new AdmCategoryAnalyze();
-		admCategoryAnalyze.setRecodeDate(new Date());
-		admCategoryAnalyze.setAdClass("0000000000001235");
-		admCategoryAnalyze.setUserIdType("UUID");
-		admCategoryAnalyze.setAdClassCountByDay(100);
-		admCategoryAnalyze.setCreateDate(new Date());
-		admCategoryAnalyze.setUdpateDate(new Date());
-		admCategoryAnalyze.setSexManCount(0);
-		admCategoryAnalyze.setSexWomanCount(0);
-		admCategoryAnalyze.setAgeRangeCount1to10(0);
-		admCategoryAnalyze.setAgeRangeCount11to20(0);
-		admCategoryAnalyze.setAgeRangeCount21to30(0);
-		admCategoryAnalyze.setAgeRangeCount31to40(0);
-		admCategoryAnalyze.setAgeRangeCount41to50(0);
-		admCategoryAnalyze.setAgeRangeCount51to60(0);
-		admCategoryAnalyze.setAgeRangeCount61to70(0);
-		admCategoryAnalyze.setAgeRangeCount71to80(0);
-		admCategoryAnalyze.setAgeRangeCount81to90(0);
-		admCategoryAnalyze.setAgeRangeCount91to100(0);
-		admCategoryAnalyzeService.save(admCategoryAnalyze);		
-		
-		System.out.println("OK");
-
-	}
 	
 	private void split(){
 //		String str="0001004706430000_UUID";
@@ -157,7 +103,16 @@ public class TestRun {
 //		String today=dateFormat.format(date);
 //		System.out.println(today);
 		
-		System.out.println(!StringUtils.equals("123", "124"));
+		
+		String age="01";
+		int ageInt=Integer.valueOf(age);
+		System.out.println("ageInt: "+ageInt);
+		
+		if ((ageInt>=1) && (ageInt<=10)){
+			System.out.println("age01to10");
+		}
+		
+//		System.out.println(!StringUtils.equals("123", "124"));
 		
 	}
 	
@@ -186,8 +141,6 @@ public class TestRun {
 //			TestRun.listTest();
 //			TestRun.hibernateDbTest2();
 //			TestRun.hibernateDbTest3();
-//			TestRun.hibernateDbTest4();
-//			TestRun.hibernateDbTest5();
 //			TestRun.split();
 			
 		} catch (Exception e) {
