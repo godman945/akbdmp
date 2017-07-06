7da0c37766e2945b5c6897c7f1af75b3baa53353
bessie <bessie@bessie6166.online.com.tw>
2017-07-06 15:30:32 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MapReduceMongoJob.java
@@ -338,23 +338,50 @@ public class MapReduceMongoJob {
 	}
 
 	public static void main(String[] args) throws Exception {
-		System.setProperty("spring.profiles.active", "stg");
+		System.setProperty("spring.profiles.active", "local");
 		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAllHadoopConfig.class);
 		IAdmCategoryAudienceAnalyzeService admCategoryAudienceAnalyzeService = ctx.getBean(IAdmCategoryAudienceAnalyzeService.class);
 
+//		List<AdmCategoryAudienceAnalyze> list=admCategoryAudienceAnalyzeService.loadAll();
+//		System.out.println("all size: "+list.size());
+//		
+//		AdmCategoryAudienceAnalyze admCategoryAudienceAnalyze = new AdmCategoryAudienceAnalyze();
+//		admCategoryAudienceAnalyze.setRecordDate(new Date());
+//		admCategoryAudienceAnalyze.setKeyId("test");
+//		admCategoryAudienceAnalyze.setKeyName("3C");
+//		admCategoryAudienceAnalyze.setKeyType("uuid");
+//		admCategoryAudienceAnalyze.setUserType("uuid");
+//		admCategoryAudienceAnalyze.setSource("24h");
+//		admCategoryAudienceAnalyze.setKeyCount(100);
+//		admCategoryAudienceAnalyze.setCreateDate(new Date());
+//		admCategoryAudienceAnalyze.setUpdateDate(new Date());
+//		admCategoryAudienceAnalyzeService.save(admCategoryAudienceAnalyze);
+//		
+//		List<AdmCategoryAudienceAnalyze> list=admCategoryAudienceAnalyzeService.loadAll();
+//		System.out.println("add size: "+list.size());
+//		
+		
+		
 		
 		SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
 		Date current = new Date();
 		String date = sdFormat.format(current);
 	    Date today = sdFormat.parse(date);;
-	    
+//	    
 	    //hibernate delete mysql today all record 
 		String query = " from AdmCategoryAudienceAnalyze where recordDate = ? ";
 	    Object[] queryParam = {today};//2017-07-05
-	    List<AdmCategoryAudienceAnalyze> list= (List<AdmCategoryAudienceAnalyze>)admCategoryAudienceAnalyzeService.findHql(query, queryParam);
-	    admCategoryAudienceAnalyzeService.deleteAll(list);
+	    List<AdmCategoryAudienceAnalyze> todayRecordList= (List<AdmCategoryAudienceAnalyze>)admCategoryAudienceAnalyzeService.findHql(query, queryParam);
+	    
+	    log.info(">>>>>> hibername delete all before : " + todayRecordList.size());
+	    admCategoryAudienceAnalyzeService.deleteAll(todayRecordList);
+	    List<AdmCategoryAudienceAnalyze> allList=admCategoryAudienceAnalyzeService.loadAll();
+	    log.info(">>>>>> hibername delete all List size after : " + allList.size());
 		
-
+	    
+	    
+	    
+//
 		final Configuration conf = new Configuration();
 		MongoConfigUtil.setInputURI(conf, "mongodb://192.168.1.37:27017/pcbappdev.class_count");
 		// conf.set("mongo.input.query",
