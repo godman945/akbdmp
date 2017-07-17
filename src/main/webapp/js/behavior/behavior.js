7b711506ac2106b549471463a93dee4bbd8a7d08
bessie <bessie@bessie6166.online.com.tw>
2017-07-17 11:29:11 +0800

    update

--- src/main/webapp/js/behavior/behavior.js
+++ src/main/webapp/js/behavior/behavior.js
@@ -1,9 +1,87 @@
-﻿//一開始就執行
+﻿var currentPage;		//全域的目前第幾頁
+var currentPageSize; 	//全域的一頁幾筆
+var currentPageText 	//全域的一頁幾筆
+var totalCount;			//總筆數
+var lastPage;			//最後一頁
+
+
+//一開始就執行
 $(document).ready(function () {
+	currentPage=Number($('#total-pages-select').find(":selected").text());
+	currentPageSize=Number($('#record-unit-select').find(":selected").text());
+	
+	//一頁幾筆select
+	$('#record-unit-select').change(function(){  
+		currentPageSize=Number($('#record-unit-select').find(":selected").text());
+		search();
+	});
+	
+	
+	//共幾頁select
+	$('#total-pages-select').change(function(){  
+		currentPage=Number($('#total-pages-select').find(":selected").text());
+		search();
+	});
+	
+	//上一頁btn
+	$('#pre-page-btn').on('click', function(){
+		if(currentPage <=1){
+			return false;
+		}
+		currentPage=Number(currentPage)-1;
+		removeSelected();
+		$("#total-pages-select option[value='"+currentPage.toString()+"']").attr("selected",true);
+		alert(currentPage);
+		search();
+	});
+	
+	//下一頁btn
+	$('#next-page-btn').on('click', function(){
+		alert("lastPage");
+		alert(lastPage);
+		if(currentPage >= lastPage){
+			return false;
+		}	
+		currentPage=Number(currentPage)+1;	
+		removeSelected();
+		$("#total-pages-select option[value='"+currentPage.toString()+"']").attr("selected",true);
+		alert(currentPage);
+		search();
+	});
+	
+	//第一頁btn
+	$('#first-page-btn').on('click', function(){
+		currentPage=Number(1);
+		removeSelected();
+		$("#total-pages-select option[value='"+currentPage.toString()+"']").attr("selected",true);
+		search();
+	});
+	
+	//最後一頁btn
+	$('#last-page-btn').on('click', function(){
+		currentPage=Number(lastPage);
+		removeSelected();
+		$("#total-pages-select option[value='"+currentPage.toString()+"']").attr("selected",true);
+		search();
+	});
+	
+	//重新查詢
+	$('#reload-btn').on('click', function(){
+		search()
+	});
 	
 });
 
+function removeSelected(){
+	$('#total-pages-select').children().each(function(){
+		$(this).attr("selected",false); //或是給selected也可
+	});
+};
+
 function search(){
+	alert("currentPage")
+	alert(currentPage)
+	
 	$("#audiencet-tbody").empty();
 	var result = null;
 	
@@ -31,6 +109,7 @@ function search(){
 		function() {
 			if(result.admCategoryAudienceAnalyzeList.length > 0){
 				var data ="";
+				var i=0;
 				$.each(result.admCategoryAudienceAnalyzeList, function(index,obj) {
 					console.log(obj);
 					
@@ -42,8 +121,10 @@ function search(){
 					var userType =  obj.userType;
 					var source =  obj.source;
 					var keyCount =  obj.keyCount;
+					i=i+1;
 					
 					data = data + '<tr id="datagrid-row-r1-2-0" datagrid-row-index="0" class="datagrid-row">'+
+					'<td field="流水號"><div style="height:auto;" class="datagrid-cell datagrid-cell-c1-itemid">'+i+'</div></td>'+
 						   '<td field="序號"><div style="height:auto;" class="datagrid-cell datagrid-cell-c1-itemid">'+id+'</div></td>'+
 						   '<td field="紀錄日期"><div style="height:auto;" class="datagrid-cell datagrid-cell-c1-itemid">'+recordDate+'</div></td>'+
 						   '<td field="分類序號"><div style="height:auto;" class="datagrid-cell datagrid-cell-c1-itemid">'+keyId+'</div></td>'+
@@ -56,12 +137,17 @@ function search(){
 				$('#audiencet-tbody').append(data);
 				console.log(result.pageSize)
 				$('#total-count').text(result.pageSize);
+				totalCount=Number(result.pageSize);
 				// 共幾頁
 				totalPages();
 			}else{
+				currentPage=Number(1);
 				$("#total-pages-select").empty();
 				$("#total-pages-select").append("<option>1</option>");
-				alert("查無資料")
+				$("#total-pages-select option[value='"+currentPage+"']").attr("selected","selected");
+				alert("查無資料");
+				alert("currentPage");
+				alert(currentPage);
 			}
 		}
 	);
@@ -69,21 +155,24 @@ function search(){
 }
 
 function totalPages(){
-	var totalPages= Math.ceil(Number($('#total-count').text())/Number($('#record-unit-select').find(":selected").text()));
-	
+	var totalPages=Number(Math.ceil(totalCount/currentPageSize));
+	lastPage=Number(totalPages);
 	var page =0;
 	var optionsAsString = "";
 	for(var i = 1; i <= totalPages; i++) {
 		page=page+1;
-	    optionsAsString += "<option value='" + page.toString() + "'>" + page.toString() + "</option>";
+	    optionsAsString += "<option value=" + page + ">" + page.toString() + "</option>";
 	}
+	
 	$("#total-pages-select").empty();
 	$("#total-pages-select").append( optionsAsString );
 	$('#total-pages-text').text(page);
-	
-	
+	removeSelected()
+	$("#total-pages-select option[value='"+currentPage+"']").attr("selected","selected");
 	return false;
 }
+
+
 
 function errMsg(){
 	$("#errMsg").remove();
