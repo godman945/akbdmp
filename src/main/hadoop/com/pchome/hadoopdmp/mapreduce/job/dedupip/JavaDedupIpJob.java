bccb56647907d4e4c26b0d3bcf4b3ac1c065dc43
bessie <bessie@bessie6166.online.com.tw>
2017-08-01 16:50:14 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dedupip/JavaDedupIpJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dedupip/JavaDedupIpJob.java
@@ -2,6 +2,8 @@ package com.pchome.hadoopdmp.mapreduce.job.dedupip;
 
 import java.io.IOException;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.LongWritable;
@@ -10,13 +12,14 @@ import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
 import com.hadoop.mapreduce.LzoTextInputFormat;
 
 public class JavaDedupIpJob {
+	
+	static Log log = LogFactory.getLog("JavaDedupIpJob");
 
 	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
 		
@@ -25,6 +28,9 @@ public class JavaDedupIpJob {
 		private Text valueOut = new Text();
 		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			
+			log.info(">>>>>> JavaDedupIpJob Mapper  setup>>>>>>>>>>>>>>>>>>>>>>>>>>");
+			log.info(">>>>>> value : "+value);
 			
 			String[] values = value.toString().split(SYMBOL);
 			
@@ -38,20 +44,24 @@ public class JavaDedupIpJob {
 	public static class Reduce extends Reducer<Text, Text, Text, Text> {
 		
 		private Text keyOut = new Text();
-		private Text valueOut = new Text();
 		int i=0;
 		
 		public void reduce(Text key, Iterable<Text> values, Context context)	throws IOException, InterruptedException {
+			log.info(">>>>>> JavaDedupIpJob reduce  setup>>>>>>>>>>>>>>>>>>>>>>>>>>");
+			log.info(">>>>>> key : "+key);
+			
 			i=i+1;
 			
 			String data = key.toString();
 
 			keyOut.set(Integer.toString(i)+"\t"+data);
-			context.write(keyOut, valueOut);
+			context.write(keyOut,new Text(""));
 		}
 	}
 
 	public static void main(String[] args) throws Exception {
+		
+		
 		Configuration conf = new Configuration();
 		Job job = new Job(conf, "Dedup");
 		job.setJarByClass(JavaDedupIpJob.class);
