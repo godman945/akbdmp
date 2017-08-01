f7a1d0ecd49ba8d8e6f2eb09b1582145994cf4eb
bessie <bessie@bessie6166.online.com.tw>
2017-08-01 16:29:50 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dedupip/JavaDedupIpJob.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dedupip/JavaDedupIpJob.java
@@ -14,6 +14,8 @@ import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 
+import com.hadoop.mapreduce.LzoTextInputFormat;
+
 public class JavaDedupIpJob {
 
 	public static class Map extends Mapper<LongWritable, Text, Text, Text> {
@@ -23,7 +25,9 @@ public class JavaDedupIpJob {
 		private Text valueOut = new Text();
 		
 		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			
 			String[] values = value.toString().split(SYMBOL);
+			
 			String result = values[3];
 			
 			keyOut.set(result);
@@ -56,7 +60,7 @@ public class JavaDedupIpJob {
 		job.setReducerClass(Reduce.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(Text.class);
-		job.setInputFormatClass(TextInputFormat.class);
+		job.setInputFormatClass(LzoTextInputFormat.class);
 		job.setOutputFormatClass(TextOutputFormat.class);
 		/* 輸入資料的HDFS路徑 */
 		FileInputFormat.addInputPath(job, new Path("/home/webuser/dmp/testData/category"));
