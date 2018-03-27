26adbc3bed2b86820a01cd5dc4b70c60df2429cb
alexchen <alexchen@alexchen11248.online.com.tw>
2018-03-27 17:55:19 +0800

    update

--- src/main/api/com/pchome/akbdmp/api/call/ad/controller/AdController.java
+++ src/main/api/com/pchome/akbdmp/api/call/ad/controller/AdController.java
@@ -67,13 +67,14 @@ public class AdController extends BaseController {
 			}
 			
 //			log.info(redisTemplate.opsForValue().get("adclass_api_"+key));
-			log.info(">>>>>>key:"+key);
+			
 			
 			
 			
 			result = (String) redisTemplate.opsForValue().get("adclass_api_"+key);
 			//呼叫kafka
 			if(StringUtils.isBlank(result)){
+				log.info(">>>>>>key:"+key);
 				result = "{\"ad_class\":[],\"behavior\":\"\",\"sex\":\"\",\"age\":\"\"}";
 				kafkaUtil.sendMessage(topicName, "", key);
 			}
