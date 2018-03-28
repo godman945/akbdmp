f50338881d3d75cab72d7622d72e98cca5050194
alexchen <alexchen@alexchen11248.online.com.tw>
2018-03-28 14:42:33 +0800

    update

--- src/main/api/com/pchome/akbdmp/api/call/ad/controller/AdclassApiThreadProcess.java
+++ src/main/api/com/pchome/akbdmp/api/call/ad/controller/AdclassApiThreadProcess.java
@@ -44,42 +44,13 @@ public class AdclassApiThreadProcess implements Callable<String> {
 
 	public synchronized String call() throws Exception {
 		long time = System.currentTimeMillis();
-		log.info(">>>>>>>>>>>>>>>map size:"+map.size());
+//		log.info(">>>>>>>>>>>>>>>map size:"+map.size());
 		map.put(String.valueOf(time), String.valueOf(time));
 		
 		
 		
+//		System.out.println(map);
 		
-		System.out.println(map);
-		
-//		//影音廣告明細總數
-//		if(enumAdThreadType.equals(EnumAdThreadType.AD_VIEW_VIDEO_COUNT)){
-//			PfpAdAdViewConditionVO pfpAdAdViewConditionVO = (PfpAdAdViewConditionVO) JSONObject.toBean(this.conditionJson, PfpAdAdViewConditionVO.class);
-//			IPfpAdService pfpAdService = this.threadServiceBean.getPfpAdService();
-//			PfpAdAdVideoViewSumVO pfpAdAdVideoViewSumVO = pfpAdService.getAdAdVideoDetailViewCount(pfpAdAdViewConditionVO);
-//			JSONObject result = JSONObject.fromObject(pfpAdAdVideoViewSumVO);
-//			return result.toString();
-//			
-//		}
-//		
-//		//影音廣告明細
-//		if(enumAdThreadType.equals(EnumAdThreadType.AD_VIEW_VIDEO_DETAIL)){
-//			PfpAdAdViewConditionVO pfpAdAdViewConditionVO = (PfpAdAdViewConditionVO) JSONObject.toBean(this.conditionJson, PfpAdAdViewConditionVO.class);
-//			IPfpAdService pfpAdService = this.threadServiceBean.getPfpAdService();
-//			List<PfpAdAdVideoViewVO> pfpAdAdVideoViewVOList = pfpAdService.getAdAdVideoDetailView(pfpAdAdViewConditionVO);	
-//			
-//			JSONArray resultList = new JSONArray();
-//			for (PfpAdAdVideoViewVO pfpAdAdVideoViewVO : pfpAdAdVideoViewVOList) {
-//				JSONObject json = JSONObject.fromObject(pfpAdAdVideoViewVO);
-//				resultList.add(json);
-//			}
-//			return resultList.toString();
-//		}
-//		
-//		//取得youtube影片網址
-//		if(enumAdThreadType.equals(EnumAdThreadType.AD_VIDEO_PLAY_URL)){
-//			
-//		}
 		return "SSS";
 	}
 }
