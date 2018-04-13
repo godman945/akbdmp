3279569a95c875639f6605b212b519cf078fd71b
alexchen <alexchen@alexchen11248.online.com.tw>
2018-04-13 12:23:52 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MongoDbDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MongoDbDriver.java
@@ -111,8 +111,11 @@ public class MongoDbDriver {
 		//prd
 //		MongoConfigUtil.setInputURI(jobConf, "mongodb://141.8.230.20:27017/dmp.user_detail");
 		MongoConfigUtil.setInputURI(jobConf,"mongodb://webuser:MonG0Dmp@mongodb.mypchome.com.tw/dmp.user_detail");
+		MongoConfigUtil.setSplitSize(jobConf, 5);
 		MongoConfigUtil.setCreateInputSplits(jobConf, false);
-
+		
+		
+		
 		final Job job = new Job(jobConf, "alex_mongo_db_log");
 		
 		
