c503a54a3fb593a389d37e4414cabac3f7c3a466
bessie <bessie@bessie6166.online.com.tw>
2018-04-16 16:42:39 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/spring/config/bean/allbeanscan/SpringAllHadoopConfig.java
+++ src/main/hadoop/com/pchome/hadoopdmp/spring/config/bean/allbeanscan/SpringAllHadoopConfig.java
@@ -24,6 +24,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 	+"com.pchome.soft.util,"
 	+"com.pchome.hadoopdmp.spring.config.bean,"
 	+"test.bessie,"
+	+"com.pchome.hadoopdmp.mapreduce.job.RawData.TEST,"
 	
 )
 @PropertySource({ 
