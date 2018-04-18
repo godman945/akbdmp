1f51f3b91c9d9ca221e864d4292a650e214a3eb5
alexchen <alexchen@alexchen11248.online.com.tw>
2018-04-18 18:26:33 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MongoDbMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mongo/job/MongoDbMapper.java
@@ -37,29 +37,29 @@ public class MongoDbMapper extends Mapper<Object, BSONObject, Text, Text> {
 			log.info(">>>>>> Mapper write value:" + value);
 			totalSizeCount.set(new Text("sizeCount"));
 			
-			context.write(new Text(String.valueOf(key+"_count")), new Text(String.valueOf(key)));
-			int range = 365;
-			
-//			Date date1 = sdf.parse(String.valueOf(value.get("create_date")));
-//			Date date2 = sdf.parse(String.valueOf(value.get("update_date")));
-			
-			Date date1 = sdf.parse(String.valueOf(value.get("update_date")));
-			Date date2 = sdf.parse(sdf.format(date));
-			
-			Calendar cal1 = Calendar.getInstance();
-	        cal1.setTime(date1);
-	        
-	        Calendar cal2 = Calendar.getInstance();
-	        cal2.setTime(date2);
-	        int rangeDay = ( int ) ((date2.getTime() - date1.getTime()) / (1000*3600*24 )); 
-			if(rangeDay > range){
-				log.info(">>>>>> Mapper write key:" + key);
-				log.info(">>>>>> rangeDay:" + rangeDay);
-//				log.info(">>>>>> Mapper write value:" + value);
-//				log.info(">>>>>> Mapper write count:" + count);
+//			context.write(new Text(String.valueOf(key+"_count")), new Text(String.valueOf(key)));
+//			int range = 365;
+//			
+////			Date date1 = sdf.parse(String.valueOf(value.get("create_date")));
+////			Date date2 = sdf.parse(String.valueOf(value.get("update_date")));
+//			
+//			Date date1 = sdf.parse(String.valueOf(value.get("update_date")));
+//			Date date2 = sdf.parse(sdf.format(date));
+//			
+//			Calendar cal1 = Calendar.getInstance();
+//	        cal1.setTime(date1);
+//	        
+//	        Calendar cal2 = Calendar.getInstance();
+//	        cal2.setTime(date2);
+//	        int rangeDay = ( int ) ((date2.getTime() - date1.getTime()) / (1000*3600*24 )); 
+//			if(rangeDay > range){
+//				log.info(">>>>>> Mapper write key:" + key);
+//				log.info(">>>>>> rangeDay:" + rangeDay);
+////				log.info(">>>>>> Mapper write value:" + value);
+////				log.info(">>>>>> Mapper write count:" + count);
 				keyOut.set(new Text(key.toString())+"_delete");
 				context.write(keyOut, new Text(value.toString()));
-			}
+//			}
 		} catch (Exception e) {
 			log.error(">>>>>> " + e.getMessage());
 		}
