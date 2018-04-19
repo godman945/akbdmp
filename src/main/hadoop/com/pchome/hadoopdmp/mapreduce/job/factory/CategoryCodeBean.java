5029f06be8a8c14d898895424085e3d15a17d20c
bessie <bessie@bessie6166.online.com.tw>
2018-04-19 10:49:12 +0800

    update

---
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/factory/CategoryCodeBean.java
@@ -0,0 +1,42 @@
+package com.pchome.hadoopdmp.mapreduce.job.factory;
+
+public class CategoryCodeBean {
+	
+    private String numberCode;
+    private String chineseDesc;
+    private String breadCrumb;
+    private String url;
+    private String englishCode;
+    
+	public String getNumberCode() {
+		return numberCode;
+	}
+	public void setNumberCode(String numberCode) {
+		this.numberCode = numberCode;
+	}
+	public String getChineseDesc() {
+		return chineseDesc;
+	}
+	public void setChineseDesc(String chineseDesc) {
+		this.chineseDesc = chineseDesc;
+	}
+	public String getBreadCrumb() {
+		return breadCrumb;
+	}
+	public void setBreadCrumb(String breadCrumb) {
+		this.breadCrumb = breadCrumb;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getEnglishCode() {
+		return englishCode;
+	}
+	public void setEnglishCode(String englishCode) {
+		this.englishCode = englishCode;
+	}
+    
+}
