46087d2b7184407f214a098ce8d3e5c8b44c0ffc
bessie <bessie@bessie6166.online.com.tw>
2018-04-23 11:51:37 +0800

    update

---
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/factory/CategoryRutenCodeBean.java
@@ -0,0 +1,21 @@
+package com.pchome.hadoopdmp.mapreduce.job.factory;
+
+public class CategoryRutenCodeBean {
+	
+    private String numberCode;
+    private String chineseDesc;
+    
+	public String getNumberCode() {
+		return numberCode;
+	}
+	public void setNumberCode(String numberCode) {
+		this.numberCode = numberCode;
+	}
+	public String getChineseDesc() {
+		return chineseDesc;
+	}
+	public void setChineseDesc(String chineseDesc) {
+		this.chineseDesc = chineseDesc;
+	}
+
+}
