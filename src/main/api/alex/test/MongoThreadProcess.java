4628a232d6739407e59965e80bdfb9cb26d6c583
alexchen <alexchen@alexchen11248.online.com.tw>
2018-04-24 12:26:05 +0800

    test

--- src/main/api/alex/test/MongoThreadProcess.java
+++ src/main/api/alex/test/MongoThreadProcess.java
@@ -16,81 +16,103 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
 import com.pchome.hadoopdmp.data.mongo.pojo.UserDetailMongoBean;
 
 
 public class MongoThreadProcess implements Callable<Integer> {
 
 	private MongoOperations mongoOperations; 
-	private int startPage;
-	private int endPage;
+	private int start;
+	private int end;
+	private DBCursor cursor;
+	private DBCollection dBCollection;
 	Log log = LogFactory.getLog(MongoThreadProcess.class);
 	
-	public MongoThreadProcess(MongoOperations mongoOperations, int startPage,int endPage) {
+	public MongoThreadProcess(int start,int end,DBCursor cursor,DBCollection dBCollection) {
 		this.mongoOperations = mongoOperations;
-		this.startPage = startPage;
-		this.endPage = endPage;
+		this.start = start;
+		this.end = end;
+		this.cursor = cursor;
+		this.dBCollection = dBCollection;
 	}
 	
 	public synchronized Integer call() throws Exception {
-		System.out.println("********** "+Thread.currentThread().getName()+"--->start");
+		System.out.println("********** "+Thread.currentThread().getName()+"--->start" + " start:"+start + " end:"+end);
+		int count = 0;
+		int index = 0;
+		while(this.cursor.hasNext()) {
+			if(index >= start && index <= end){
+				DBObject a = cursor.next();
+				if(this.dBCollection.findOne(a) != null){
+					System.out.println(Thread.currentThread().getName()+ " delete " +a.get("_id"));
+					this.dBCollection.remove(a);
+					count = count + 1;
+				}
+			}
+			index ++;
+		}
+		
+		
 //		System.out.println("********** "+Thread.currentThread().getName()+"--->startPage:"+startPage);
 //		System.out.println("********** "+Thread.currentThread().getName()+"--->endPage:"+endPage);
 		
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-		int page = startPage;
-		int size = 10000;
-		int range = 365;
-		int count = 0;
-		long time1, time2;
-		
-		boolean flag = true;
-		while(flag){
-			time1 = System.currentTimeMillis();
-			Pageable pageableRequest = new PageRequest(page, size);
-			Query query = new Query();
-			query.addCriteria(Criteria.where("user_info.type").is("uuid"));
-			query.with(pageableRequest);
-			query.with(new Sort(Direction.DESC, "_id"));
-			List<UserDetailMongoBean> listUser = mongoOperations.find(query,UserDetailMongoBean.class);
-			int processSize = listUser.size();
-			
-			
-//			System.out.println("********** "+Thread.currentThread().getName()+"--->page:"+page);
-//			System.out.println("********** "+Thread.currentThread().getName()+"--->size:"+processSize);
-			if(processSize == 0){
-				flag = false;
-			}
-			
-			for (UserDetailMongoBean userDetailMongoBean : listUser) {
-				Date date1 = sdf.parse(userDetailMongoBean.getCreate_date());
-				Date date2 = sdf.parse(userDetailMongoBean.getUpdate_date());
-				
-				Calendar cal1 = Calendar.getInstance();
-		        cal1.setTime(date1);
-		        
-		        Calendar cal2 = Calendar.getInstance();
-		        cal2.setTime(date2);
-		        int rangeDay = ( int ) ((date2.getTime() - date1.getTime()) / (1000*3600*24 )); 
-				if(rangeDay > range){
-					count = count + 1;
-//					System.out.println("********** need delete start **********");
-//					System.out.println("*************差異天數:" +rangeDay);
-//					System.out.println(userDetailMongoBean.get_id());
-//					mongoOperations.remove(Query.query(Criteria.where("_id").is(userDetailMongoBean.get_id())), UserDetailMongoBean.class);
-//					System.out.println("********** need delete end **********");
-				}
-			}
-			log.info("********** "+Thread.currentThread().getName()+" ********************* process page:" + page);
-			page = page + 1;
-			
-			log.info("**********" +Thread.currentThread().getName()+":need delete count:"+count);
-			time2 = System.currentTimeMillis();
-			log.info(Thread.currentThread().getName()+" >>>>>>>> cost " + (time2-time1)/1000 + " sec");
-			if(page > endPage){
-				flag = false;
-			}
-		}
+//		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+//		int page = startPage;
+//		int size = 10000;
+//		int range = 365;
+//		int count = 0;
+//		long time1, time2;
+//		
+//		boolean flag = true;
+//		while(flag){
+//			time1 = System.currentTimeMillis();
+//			Pageable pageableRequest = new PageRequest(page, size);
+//			Query query = new Query();
+//			query.addCriteria(Criteria.where("user_info.type").is("uuid"));
+//			query.with(pageableRequest);
+//			query.with(new Sort(Direction.DESC, "_id"));
+//			List<UserDetailMongoBean> listUser = mongoOperations.find(query,UserDetailMongoBean.class);
+//			int processSize = listUser.size();
+//			
+//			
+////			System.out.println("********** "+Thread.currentThread().getName()+"--->page:"+page);
+////			System.out.println("********** "+Thread.currentThread().getName()+"--->size:"+processSize);
+//			if(processSize == 0){
+//				flag = false;
+//			}
+//			
+//			for (UserDetailMongoBean userDetailMongoBean : listUser) {
+//				Date date1 = sdf.parse(userDetailMongoBean.getCreate_date());
+//				Date date2 = sdf.parse(userDetailMongoBean.getUpdate_date());
+//				
+//				Calendar cal1 = Calendar.getInstance();
+//		        cal1.setTime(date1);
+//		        
+//		        Calendar cal2 = Calendar.getInstance();
+//		        cal2.setTime(date2);
+//		        int rangeDay = ( int ) ((date2.getTime() - date1.getTime()) / (1000*3600*24 )); 
+//				if(rangeDay > range){
+//					count = count + 1;
+////					System.out.println("********** need delete start **********");
+////					System.out.println("*************差異天數:" +rangeDay);
+////					System.out.println(userDetailMongoBean.get_id());
+////					mongoOperations.remove(Query.query(Criteria.where("_id").is(userDetailMongoBean.get_id())), UserDetailMongoBean.class);
+////					System.out.println("********** need delete end **********");
+//				}
+//			}
+//			log.info("********** "+Thread.currentThread().getName()+" ********************* process page:" + page);
+//			page = page + 1;
+//			
+//			log.info("**********" +Thread.currentThread().getName()+":need delete count:"+count);
+//			time2 = System.currentTimeMillis();
+//			log.info(Thread.currentThread().getName()+" >>>>>>>> cost " + (time2-time1)/1000 + " sec");
+//			if(page > endPage){
+//				flag = false;
+//			}
+//		}
 		return count;
 	}
 }
--- src/main/api/alex/test/TEST.java
+++ src/main/api/alex/test/TEST.java
@@ -1,5 +1,7 @@
 package alex.test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.ThreadPoolExecutor;
@@ -9,6 +11,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.data.mongodb.core.MongoOperations;
 
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBCursor;
+import com.mongodb.DBObject;
+import com.mongodb.Mongo;
 import com.pchome.akbdmp.spring.config.bean.allbeanscan.SpringAllConfig;
 
 
@@ -16,70 +24,112 @@ public class TEST {
 	static Log log = LogFactory.getLog(TEST.class);
 	
 	public static void main(String[] args) throws Exception {
-		System.setProperty("spring.profiles.active", "prd");
-		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAllConfig.class);
-		MongoOperations mongoOperations = (MongoOperations) ctx.getBean(MongoOperations.class);
+//		System.setProperty("spring.profiles.active", "prd");
+//		ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAllConfig.class);
+//		MongoOperations mongoOperations = (MongoOperations) ctx.getBean(MongoOperations.class);
+		
+		long time1, time2;
+		time1 = System.currentTimeMillis();
+		Mongo m = new Mongo("mongodb.mypchome.com.tw");  
+		DB db = m.getDB("dmp");
+		db.authenticate("webuser", "MonG0Dmp".toCharArray());  
+		DBCollection dBCollection = db.getCollection("user_detail");
+//		DBCollection dBCollection = db.getCollection("class_url");
+		
+		BasicDBObject andQuery = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+//		obj.add(new BasicDBObject("update_date", new BasicDBObject("$lt", "2017-04-24")));
+		obj.add(new BasicDBObject("update_date", new BasicDBObject("$lt", "2017-04-24")));
+		obj.add(new BasicDBObject("user_info.type", "uuid"));
+		andQuery.put("$and", obj);
+//		andQuery.put("update_date", new BasicDBObject("$lte", "2017-04-18"));
+		System.out.println(andQuery.toString());
+		DBCursor cursor = dBCollection.find(andQuery);
 		
 		
-		/**多執行緒*/
-		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
 		
-		Future<Integer> mongoThreadProcess1Result = null;
-		MongoThreadProcess mongoThreadProcess1 = new MongoThreadProcess(mongoOperations,0,999);
-		mongoThreadProcess1Result = executor.submit(mongoThreadProcess1);
+		System.out.println(cursor.count());
+		time2 = System.currentTimeMillis();
+		log.info(" >>>>>>>> cost " + (time2-time1)/1000 + " sec");
 		
-		Future<Integer> mongoThreadProcess2Result = null;
-		MongoThreadProcess mongoThreadProcess2 = new MongoThreadProcess(mongoOperations,1000,1999);
-		mongoThreadProcess2Result = executor.submit(mongoThreadProcess2);
+//		while(cursor.hasNext()) {
+//			DBObject a = cursor.next();
+//			System.out.println(a.get("_date"));
+////			if(dBCollection.findOne(a) != null){
+////				System.out.println("delete:" +a.get("_id"));
+////				dBCollection.remove(a);
+////			}
+//		}
 		
-		Future<Integer> mongoThreadProcess3Result = null;
-		MongoThreadProcess mongoThreadProcess3 = new MongoThreadProcess(mongoOperations,2000,2999);
-		mongoThreadProcess3Result = executor.submit(mongoThreadProcess3);
-		
-		Future<Integer> mongoThreadProcess4Result = null;
-		MongoThreadProcess mongoThreadProcess4 = new MongoThreadProcess(mongoOperations,3000,5000);
-		mongoThreadProcess4Result = executor.submit(mongoThreadProcess4);
+//		{ "$and" : [ { "update_date" : { "$lt" : "2017-04-24"}} , { "user_info.type" : "uuid"}]}
+//		69805
+//		2018/04/24 10:45:12 [INFO ] [alex.test.TEST@main]main(51)  >>>>>>>> cost 425 sec
 		
 		
-		boolean mongoThreadProcess1ResultFlag = true;
-		boolean mongoThreadProcess2ResultFlag = true;
-		boolean mongoThreadProcess3ResultFlag = true;
-		boolean mongoThreadProcess4ResultFlag = true;
 		
-		int totalDelete = 0;
-		while (mongoThreadProcess1ResultFlag || mongoThreadProcess2ResultFlag || mongoThreadProcess3ResultFlag || mongoThreadProcess4ResultFlag) {
-			if (mongoThreadProcess1Result.isDone() && mongoThreadProcess1ResultFlag) {
-				int result = mongoThreadProcess1Result.get();
-				log.info("thread-1 total finish --->need delete count:"+result);
-				totalDelete = totalDelete + result;
-				mongoThreadProcess1ResultFlag = false;
-			}
-			
-			if (mongoThreadProcess2Result.isDone() && mongoThreadProcess2ResultFlag) {
-				int result = mongoThreadProcess2Result.get();
-				log.info("thread-2 total finish --->need delete count:"+result);
-				totalDelete = totalDelete + result;
-				mongoThreadProcess2ResultFlag = false;
-			}
-			
-			if (mongoThreadProcess3Result.isDone() && mongoThreadProcess3ResultFlag) {
-				int result = mongoThreadProcess3Result.get();
-				log.info("thread-3 total finish --->need delete count:"+result);
-				totalDelete = totalDelete + result;
-				mongoThreadProcess3ResultFlag = false;
-			}
-			
-			if (mongoThreadProcess4Result.isDone() && mongoThreadProcess4ResultFlag) {
-				int result = mongoThreadProcess4Result.get();
-				log.info("thread-4 total finish --->need delete count:"+result);
-				totalDelete = totalDelete + result;
-				mongoThreadProcess4ResultFlag = false;
-			}
-			
-		}
 		
-		log.info(">>>>>>>>>>>>>>>>>>>> all thread finish totalDelete:"+totalDelete);
-		executor.shutdown();
+		
+		
+//		
+//		/**多執行緒*/
+//		ThreadPoolExecutor executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(10);
+//		
+//		Future<Integer> mongoThreadProcess1Result = null;
+//		MongoThreadProcess mongoThreadProcess1 = new MongoThreadProcess(0,9999999,cursor,dBCollection);
+//		mongoThreadProcess1Result = executor.submit(mongoThreadProcess1);
+//		
+//		Future<Integer> mongoThreadProcess2Result = null;
+//		MongoThreadProcess mongoThreadProcess2 = new MongoThreadProcess(1000000,1999999,cursor,dBCollection);
+//		mongoThreadProcess2Result = executor.submit(mongoThreadProcess2);
+//		
+//		Future<Integer> mongoThreadProcess3Result = null;
+//		MongoThreadProcess mongoThreadProcess3 = new MongoThreadProcess(2000000,2999999,cursor,dBCollection);
+//		mongoThreadProcess3Result = executor.submit(mongoThreadProcess3);
+//		
+//		Future<Integer> mongoThreadProcess4Result = null;
+//		MongoThreadProcess mongoThreadProcess4 = new MongoThreadProcess(3000000,3999999,cursor,dBCollection);
+//		mongoThreadProcess4Result = executor.submit(mongoThreadProcess4);
+//		
+//		
+//		boolean mongoThreadProcess1ResultFlag = true;
+//		boolean mongoThreadProcess2ResultFlag = true;
+//		boolean mongoThreadProcess3ResultFlag = true;
+//		boolean mongoThreadProcess4ResultFlag = true;
+//		
+//		int totalDelete = 0;
+//		while (mongoThreadProcess1ResultFlag || mongoThreadProcess2ResultFlag || mongoThreadProcess3ResultFlag || mongoThreadProcess4ResultFlag) {
+//			if (mongoThreadProcess1Result.isDone() && mongoThreadProcess1ResultFlag) {
+//				int result = mongoThreadProcess1Result.get();
+//				log.info("thread-1 total finish --->need delete count:"+result);
+//				totalDelete = totalDelete + result;
+//				mongoThreadProcess1ResultFlag = false;
+//			}
+//			
+//			if (mongoThreadProcess2Result.isDone() && mongoThreadProcess2ResultFlag) {
+//				int result = mongoThreadProcess2Result.get();
+//				log.info("thread-2 total finish --->need delete count:"+result);
+//				totalDelete = totalDelete + result;
+//				mongoThreadProcess2ResultFlag = false;
+//			}
+//			
+//			if (mongoThreadProcess3Result.isDone() && mongoThreadProcess3ResultFlag) {
+//				int result = mongoThreadProcess3Result.get();
+//				log.info("thread-3 total finish --->need delete count:"+result);
+//				totalDelete = totalDelete + result;
+//				mongoThreadProcess3ResultFlag = false;
+//			}
+//			
+//			if (mongoThreadProcess4Result.isDone() && mongoThreadProcess4ResultFlag) {
+//				int result = mongoThreadProcess4Result.get();
+//				log.info("thread-4 total finish --->need delete count:"+result);
+//				totalDelete = totalDelete + result;
+//				mongoThreadProcess4ResultFlag = false;
+//			}
+//			
+//		}
+//		
+//		log.info(">>>>>>>>>>>>>>>>>>>> all thread finish totalDelete:"+totalDelete);
+//		executor.shutdown();
 	}
 
 }
