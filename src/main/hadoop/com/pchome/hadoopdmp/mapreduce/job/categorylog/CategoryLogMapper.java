f5d9b130ea2ec4966e3a4593bb4e1becff9ad330
bessie <bessie@bessie6166.online.com.tw>
2018-05-03 15:40:29 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/categorylog/CategoryLogMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/categorylog/CategoryLogMapper.java
@@ -189,7 +189,6 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 			}
 			
 
-			CategoryLogBean categoryLogBean = new CategoryLogBean();
 			CategoryLogBean categoryLogBeanResult = null;
 			
 			//分析click、24H、Ruten、campaign分類 
@@ -206,13 +205,9 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 				return;
 			}
 			
-//			//分類物件為null，不往下做
-//			if (categoryLogBeanResult == null) {
-//				return;
-//			}
 			
 			//處理個資
-			categoryLogBeanResult = personalInfoComponent.processPersonalInfo(categoryLogBeanResult, mongoOperations);
+//			categoryLogBeanResult = personalInfoComponent.processPersonalInfo(categoryLogBeanResult, mongoOperations);
 							
 			
 			categoryLogBeanResult.setRecodeDate(record_date);
@@ -221,18 +216,14 @@ public class CategoryLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 			// 7.PersonalInfoMemberApiClassify(打會員api是否有完整個資 Y/N) + 8.Sex(推估性別) + 9.Age(推估年齡) +
 			// 10.PersonalInfoClassify(依adClass比對分類年齡性別對應表ClsfyGndAgeCrspTable，是否有完整的推估個資 Y/N)
 			String memid = StringUtils.isBlank(categoryLogBeanResult.getMemid()) ? "null" : categoryLogBeanResult.getMemid();
+//			String result = memid + SYMBOL + categoryLogBeanResult.getUuid() + SYMBOL + categoryLogBeanResult.getAdClass() + SYMBOL  + dmpDataBean.getUrl() 
+//							+ SYMBOL + categoryLogBeanResult.getMsex() + SYMBOL + categoryLogBeanResult.getMage()+ SYMBOL + categoryLogBeanResult.getSource()   
+//							+ SYMBOL + categoryLogBeanResult.getSex() + SYMBOL + categoryLogBeanResult.getAge(); 
 			String result = memid + SYMBOL + categoryLogBeanResult.getUuid() + SYMBOL + categoryLogBeanResult.getAdClass() + SYMBOL  + dmpDataBean.getUrl() 
-							+ SYMBOL + categoryLogBeanResult.getMsex() + SYMBOL + categoryLogBeanResult.getMage()+ SYMBOL + categoryLogBeanResult.getSource()   
-							+ SYMBOL + categoryLogBeanResult.getSex() + SYMBOL + categoryLogBeanResult.getAge(); 
-							
+			+ SYMBOL + categoryLogBeanResult.getSource(); 
+			
 			
 			log.info(">>>>>> Mapper write key:" + result);
-			
-			
-			/*舊版			
-			 0:Memid + 1:Uuid + 2:AdClass + 3:Age + 4:Sex + 5:Source + 6:RecodeDate + 7:Type + 8:Classify
-			String result = memid + SYMBOL + categoryLogBeanResult.getUuid() + SYMBOL + categoryLogBeanResult.getAdClass() + SYMBOL + categoryLogBeanResult.getAge() + SYMBOL + categoryLogBeanResult.getSex() + SYMBOL + categoryLogBeanResult.getSource()+ SYMBOL +categoryLogBeanResult.getRecodeDate() + SYMBOL + categoryLogBeanResult.getType() + SYMBOL + values[4] + SYMBOL + categoryLogBeanResult.getType()+"_"+categoryLogBeanResult.getSource()+"_"+categoryLogBeanResult.getBehaviorClassify() + SYMBOL + "user_info_Classify_"+categoryLogBeanResult.getPersonalInfoClassify();
-			 */	
 			
 			keyOut.set(result);
 			context.write(keyOut, valueOut);
