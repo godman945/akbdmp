594a0116c3b42c18437997a99aeb83877f36875f
bessie <bessie@bessie6166.online.com.tw>
2018-05-23 09:40:04 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
@@ -204,8 +204,8 @@ public class DmpLogDriver {
 				//輸入
 //				String logInputPath = "/home/webuser/dmp/testData/category";						//測試path有ruten
 //				String logInputPath = "/home/webuser/akb/storedata/alllog/2018-05-18/06";			//測試path
-//				String logInputPath = "/home/webuser/analyzer/storedata/alllog/2018-05-21";			//測試path整天log
-				String logInputPath = akbPathAllLog + timePath; //正式path  /home/webuser/akb/storedata/alllog/2018-05-15/05    	//正式path
+				String logInputPath = "/home/webuser/analyzer/storedata/alllog/2018-05-22";			//測試path整天log
+//				String logInputPath = akbPathAllLog + timePath; //正式path  /home/webuser/akb/storedata/alllog/2018-05-15/05    	//正式path
 				//輸出
 				String outputTempPath = "/home/webuser/bessie/output";
 				//hdfs存在則刪除
