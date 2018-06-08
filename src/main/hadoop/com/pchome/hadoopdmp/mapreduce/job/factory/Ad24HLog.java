1d2774b8a9178e58798990664442fec1b560eb37
bessie <bessie@bessie6166.online.com.tw>
2018-06-08 10:59:01 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/factory/Ad24HLog.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/factory/Ad24HLog.java
@@ -6,9 +6,6 @@ import java.util.Date;
 import java.util.List;
 
 import org.apache.commons.lang.StringUtils;
-import org.springframework.data.mongodb.core.query.Criteria;
-import org.springframework.data.mongodb.core.query.Query;
-import org.springframework.data.mongodb.core.query.Update;
 
 import com.mongodb.BasicDBObject;
 import com.mongodb.DB;
@@ -16,15 +13,15 @@ import com.mongodb.DBCollection;
 import com.mongodb.DBObject;
 import com.pchome.hadoopdmp.dao.IClassUrlDAO;
 import com.pchome.hadoopdmp.dao.mongodb.ClassUrlDAO;
-import com.pchome.hadoopdmp.data.mongo.pojo.ClassUrlMongoBean;
 import com.pchome.hadoopdmp.mapreduce.job.dmplog.DmpLogMapper;
 
 
 public class Ad24HLog extends ACategoryLogData {
-	
 	private IClassUrlDAO daoClassUrl = ClassUrlDAO.getInstance();
+	private DBCollection dBCollection;
 	
 	public Object processCategory(DmpLogBean dmpDataBean, DB mongoOperations) throws Exception {
+		this.dBCollection= mongoOperations.getCollection("class_url");
 		
 		dmpDataBean.setSource("kdcl");
 		
@@ -53,14 +50,14 @@ public class Ad24HLog extends ACategoryLogData {
 		}
 		
 		if (StringUtils.isBlank(category)){
-			//new
-			DBCollection dBCollection = mongoOperations.getCollection("class_url");
-			BasicDBObject andQuery = new BasicDBObject();
-			List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
-			obj.add(new BasicDBObject("url", sourceUrl.trim()));
-			andQuery.put("$and", obj);
-			DBObject dbObject =  dBCollection.findOne(andQuery);
-			//new
+//			DBCollection dBCollection = mongoOperations.getCollection("class_url");
+//			BasicDBObject andQuery = new BasicDBObject();
+//			List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+//			obj.add(new BasicDBObject("url", sourceUrl.trim()));
+//			andQuery.put("$and", obj);
+//			DBObject dbObject =  dBCollection.findOne(andQuery);
+			
+			DBObject dbObject =queryClassUrl( mongoOperations,url) ;
 			
 			if(dbObject != null){
 				if(dbObject.get("status").equals("0")){
@@ -69,13 +66,14 @@ public class Ad24HLog extends ACategoryLogData {
 					categorySource = "null";
 					class24hUrlClassify = "N";
 					
+					updateClassUrl(mongoOperations,url,dbObject) ;
+					
+					
 					SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 					Date today = new Date();
 					String todayStr = sdf.format(today);
-					
 //					Date updateDate = dbObject.get("update_date");
 					String updateDateStr = sdf.format(dbObject.get("update_date"));
-					
 					if ( (!todayStr.equals(updateDateStr)) ){
 						Date date = new Date();
 						dbObject.put("update_date", date);
@@ -97,6 +95,10 @@ public class Ad24HLog extends ACategoryLogData {
 						filter.put("url", sourceUrl.trim());
 						dBCollection.update(filter,newDocument);
 					}
+					
+					
+					
+					
 				}else if( (dbObject.get("status").equals("1")) && (StringUtils.isNotBlank(dbObject.get("ad_class").toString())) ){
 					//url 存在 status = 1 取分類代號回傳 mongo update_date 更新(一天一次) class24hUrlClassify = "Y"
 					category = dbObject.get("ad_class").toString();
@@ -152,12 +154,48 @@ public class Ad24HLog extends ACategoryLogData {
 			}
 		}
 		
-		
 		dmpDataBean.setCategory(category);
 		dmpDataBean.setCategorySource(categorySource);
 		dmpDataBean.setClass24hUrlClassify(class24hUrlClassify);
 		
 		return dmpDataBean;
+	}
+	
+	
+
+	public DBObject queryClassUrl(DB mongoOperations,String url) throws Exception {
+		BasicDBObject andQuery = new BasicDBObject();
+		List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+		obj.add(new BasicDBObject("url",url));
+		andQuery.put("$and", obj);
+		DBObject dbObject =  dBCollection.findOne(andQuery);
+		return dbObject;
+	}
+	
+	public void updateClassUrl(DB mongoOperations,String url,DBObject dbObject) throws Exception {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		Date today = new Date();
+		String todayStr = sdf.format(today);
+		String updateDateStr = sdf.format(dbObject.get("update_date"));
+		if ( (!todayStr.equals(updateDateStr)) ){
+			Date date = new Date();
+			dbObject.put("update_date", date);
+
+		    DBObject olddbObject = new BasicDBObject();
+		    olddbObject.put("url", sourceUrl.trim());
+		    dBCollection.update(olddbObject, dbObject);
+		}
+		if ( (Integer.parseInt( dbObject.get("query_time").toString()) <2000) ){
+//			Update querytime = new Update();
+//			querytime.inc( "query_time" , 1 );
+//			mongoOperations.updateFirst(new Query(Criteria.where( "url" ).is(sourceUrl.trim())), querytime, "class_url");
+			
+			BasicDBObject newDocument = new BasicDBObject();
+			newDocument.append("$inc", new BasicDBObject().append("query_time", 1));
+			DBObject filter = new BasicDBObject(); 
+			filter.put("url", sourceUrl.trim());
+			dBCollection.update(filter,newDocument);
+		}
 	}
 	
 }
