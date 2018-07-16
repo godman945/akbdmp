6fc33163b4ecbba458a7b8aaa09b2c187fbeefc1
bessie <bessie@bessie6166.online.com.tw>
2018-07-16 16:09:55 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/thirdcategorylog/ThirdCategoryLogMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/thirdcategorylog/ThirdCategoryLogMapper.java
@@ -251,13 +251,13 @@ public class ThirdCategoryLogMapper extends Mapper<LongWritable, Text, Text, Tex
 			//分類處理元件(分析click、24H、Ruten、campaign分類) 
 			if ( (dmpDataBean.getSource().equals("ck")||dmpDataBean.getSource().equals("campaign")) ) {	// kdcl ad_click的adclass  或   campaign log的adclass 	//&& StringUtils.isNotBlank(dmpLogBeanResult.getAdClass())
 				ACategoryLogData aCategoryLogData = CategoryLogFactory.getACategoryLogObj(CategoryLogEnum.AD_CLICK);
-				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpLogBeanResult, mongoOrgOperations);
+				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpDataBean, mongoOrgOperations);
 			}else if (dmpDataBean.getSource().equals("pv") && StringUtils.isNotBlank(dmpDataBean.getUrl()) && dmpDataBean.getUrl().contains("ruten")) {	// 露天
 				ACategoryLogData aCategoryLogData = CategoryLogFactory.getACategoryLogObj(CategoryLogEnum.PV_RETUN);
-				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpLogBeanResult, mongoOrgOperations);
+				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpDataBean, mongoOrgOperations);
 			}else if (dmpDataBean.getSource().equals("pv") && StringUtils.isNotBlank(dmpDataBean.getUrl()) && dmpDataBean.getUrl().contains("24h")) {		// 24h
 				ACategoryLogData aCategoryLogData = CategoryLogFactory.getACategoryLogObj(CategoryLogEnum.PV_24H);
-				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpLogBeanResult, mongoOrgOperations);
+				dmpLogBeanResult = (DmpLogBean) aCategoryLogData.processCategory(dmpDataBean, mongoOrgOperations);
 			}else if ( dmpDataBean.getSource().equals("pv") ){
 				dmpLogBeanResult.setSource("kdcl");
 				dmpLogBeanResult.setCategory("null");
@@ -273,9 +273,9 @@ public class ThirdCategoryLogMapper extends Mapper<LongWritable, Text, Text, Tex
 			//紀錄日期
 			dmpLogBeanResult.setRecordDate(record_date);
 			
-			log.info(">>>>>>category:" + dmpLogBeanResult.getCategory());
+//			log.info(">>>>>>category:" + dmpLogBeanResult.getCategory());
+//			log.info(">>>>>>CategorySource:" + dmpLogBeanResult.getCategorySource());
 			
-			log.info(">>>>>>CategorySource:" + dmpLogBeanResult.getCategorySource());
 			
 			//傳至kafka的值只要是null或空字串，全部轉成字串null
 			dmpLogBeanResult = dmpBeanIntegrate(dmpLogBeanResult);
