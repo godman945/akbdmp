7a7657a20af37b440b16e3a5eb8fa005682b17cc
bessie <bessie@bessie6166.online.com.tw>
2018-07-18 12:18:00 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/thirdcategorylog/ThirdCategoryLogMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/thirdcategorylog/ThirdCategoryLogMapper.java
@@ -52,97 +52,97 @@ public class ThirdCategoryLogMapper extends Mapper<LongWritable, Text, Text, Tex
 	private Text keyOut = new Text();
 	private Text valueOut = new Text();
 
-//	public static String record_date;
-//	public static ArrayList<Map<String, String>> categoryList = new ArrayList<Map<String, String>>();		     //分類表	
-//	public static Map<String, combinedValue> clsfyCraspMap = new HashMap<String, combinedValue>();				 //分類個資表
-//	public static List<CategoryCodeBean> category24hBeanList = new ArrayList<CategoryCodeBean>();				 //24H分類表
-//	public static List<CategoryRutenCodeBean> categoryRutenBeanList = new ArrayList<CategoryRutenCodeBean>();	 //Ruten分類表
-//	public static ArrayList<String> prodFileList = new ArrayList<String>();	 								     //24h、ruten第3分類對照表
-//	public static ThirdAdClassComponent thirdAdClassComponent = new ThirdAdClassComponent();
-//	public static PersonalInfoComponent personalInfoComponent = new PersonalInfoComponent();
-//	public static GeoIpComponent geoIpComponent = new GeoIpComponent();
-//	public static DateTimeComponent dateTimeComponent = new DateTimeComponent();
-//	public static DeviceComponent deviceComponent = new DeviceComponent();
-//	private DB mongoOrgOperations;
-//	public static DatabaseReader reader = null;
-//	public static InetAddress ipAddress = null;
+	public static String record_date;
+	public static ArrayList<Map<String, String>> categoryList = new ArrayList<Map<String, String>>();		     //分類表	
+	public static Map<String, combinedValue> clsfyCraspMap = new HashMap<String, combinedValue>();				 //分類個資表
+	public static List<CategoryCodeBean> category24hBeanList = new ArrayList<CategoryCodeBean>();				 //24H分類表
+	public static List<CategoryRutenCodeBean> categoryRutenBeanList = new ArrayList<CategoryRutenCodeBean>();	 //Ruten分類表
+	public static ArrayList<String> prodFileList = new ArrayList<String>();	 								     //24h、ruten第3分類對照表
+	public static ThirdAdClassComponent thirdAdClassComponent = new ThirdAdClassComponent();
+	public static PersonalInfoComponent personalInfoComponent = new PersonalInfoComponent();
+	public static GeoIpComponent geoIpComponent = new GeoIpComponent();
+	public static DateTimeComponent dateTimeComponent = new DateTimeComponent();
+	public static DeviceComponent deviceComponent = new DeviceComponent();
+	private DB mongoOrgOperations;
+	public static DatabaseReader reader = null;
+	public static InetAddress ipAddress = null;
 
 	@Override
 	public void setup(Context context) {
 		log.info(">>>>>> Third Category Mapper  setup >>>>>>>>>>>>>>env>>>>>>>>>>>>"+context.getConfiguration().get("spring.profiles.active"));
-//		try {
-//			System.setProperty("spring.profiles.active", context.getConfiguration().get("spring.profiles.active"));
-//			ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAllHadoopConfig.class);
-//			this.mongoOrgOperations = ctx.getBean(MongodbOrgHadoopConfig.class).mongoProducer();
-//			record_date = context.getConfiguration().get("job.date");
-//			Configuration conf = context.getConfiguration();
-//			
-//			//load 推估分類個資表(ClsfyGndAgeCrspTable.txt)
-//			org.apache.hadoop.fs.Path[] path = DistributedCache.getLocalCacheFiles(conf);
-//			Path clsfyTable = Paths.get(path[1].toString());
-//			Charset charset = Charset.forName("UTF-8");
-//			List<String> lines = Files.readAllLines(clsfyTable, charset);
-//			for (String line : lines) {
-//				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
-//				String[] tmpStrAry2 = tmpStrAry[1].split(",");
-//				clsfyCraspMap.put(tmpStrAry[0],new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
-//			}
-//			
-//
-//			// load 分類表(pfp_ad_category_new.csv)
-//			Path cate_path = Paths.get(path[0].toString());
-//			charset = Charset.forName("UTF-8");
-//			int maxCateLvl = 4;
-//			categoryList = new ArrayList<Map<String, String>>();
-//			for (int i = 0; i < maxCateLvl; i++) {
-//				categoryList.add(new HashMap<String, String>());
-//			}
-//			lines.clear();
-//			lines = Files.readAllLines(cate_path, charset);
-//			// 將 table: pfp_ad_category_new 內容放入list中(共有 maxCateLvl 層)
-//			for (String line : lines) {
-//				String[] tmpStr = line.split(";");
-//				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
-//				if (lvl <= maxCateLvl) {
-//					categoryList.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
-//				}
-//			}
-//			
-//			// load 24h分類表(DMP_24h_category.csv)
-//			Path category24HPath = Paths.get(path[3].toString());
-//			List<String> lines24H = Files.readAllLines(category24HPath, charset);
-//			for (String line : lines24H) {
-//				CategoryCodeBean categoryBean = new CategoryCodeBean();
-//				
-//				String[] tmpStrAry = line.split(","); // 0001000000000000;M,35
-//				
-//				categoryBean.setNumberCode(tmpStrAry[0].replaceAll("\"", ""));
-//				categoryBean.setChineseDesc(tmpStrAry[1].replaceAll("\"", ""));
-//				categoryBean.setBreadCrumb(tmpStrAry[2].replaceAll("\"", ""));
-//				categoryBean.setEnglishCode(tmpStrAry[3].replaceAll("\"", ""));
-//				
-//				category24hBeanList.add(categoryBean);
-//			}
-//			
-//			// load Ruten分類表(DMP_Ruten_category.csv)
-//			Path categoryRutenPath = Paths.get(path[4].toString());
-//			List<String> linesRuten = Files.readAllLines(categoryRutenPath, charset);
-//			for (String line : linesRuten) {
-//				CategoryRutenCodeBean categoryRutenBean = new CategoryRutenCodeBean();
-//				
-//				String[] tmpStrAry = line.split(","); //"0001000000000000","電腦、電腦周邊"
-//				
-//				categoryRutenBean.setNumberCode(tmpStrAry[0].replaceAll("\"", ""));
-//				categoryRutenBean.setChineseDesc(tmpStrAry[1].replaceAll("\"", ""));
-//				
-//				categoryRutenBeanList.add(categoryRutenBean);
-//			}
-//			
-//			//IP轉城市
-//			File database = new File(path[5].toString());
-//			reader = new DatabaseReader.Builder(database).build();  
-//			
-//			
+		try {
+			System.setProperty("spring.profiles.active", context.getConfiguration().get("spring.profiles.active"));
+			ApplicationContext ctx = new AnnotationConfigApplicationContext(SpringAllHadoopConfig.class);
+			this.mongoOrgOperations = ctx.getBean(MongodbOrgHadoopConfig.class).mongoProducer();
+			record_date = context.getConfiguration().get("job.date");
+			Configuration conf = context.getConfiguration();
+			
+			//load 推估分類個資表(ClsfyGndAgeCrspTable.txt)
+			org.apache.hadoop.fs.Path[] path = DistributedCache.getLocalCacheFiles(conf);
+			Path clsfyTable = Paths.get(path[1].toString());
+			Charset charset = Charset.forName("UTF-8");
+			List<String> lines = Files.readAllLines(clsfyTable, charset);
+			for (String line : lines) {
+				String[] tmpStrAry = line.split(";"); // 0001000000000000;M,35
+				String[] tmpStrAry2 = tmpStrAry[1].split(",");
+				clsfyCraspMap.put(tmpStrAry[0],new combinedValue(tmpStrAry[1].split(",")[0], tmpStrAry2.length > 1 ? tmpStrAry2[1] : ""));
+			}
+			
+
+			// load 分類表(pfp_ad_category_new.csv)
+			Path cate_path = Paths.get(path[0].toString());
+			charset = Charset.forName("UTF-8");
+			int maxCateLvl = 4;
+			categoryList = new ArrayList<Map<String, String>>();
+			for (int i = 0; i < maxCateLvl; i++) {
+				categoryList.add(new HashMap<String, String>());
+			}
+			lines.clear();
+			lines = Files.readAllLines(cate_path, charset);
+			// 將 table: pfp_ad_category_new 內容放入list中(共有 maxCateLvl 層)
+			for (String line : lines) {
+				String[] tmpStr = line.split(";");
+				int lvl = Integer.parseInt(tmpStr[5].replaceAll("\"", "").trim());
+				if (lvl <= maxCateLvl) {
+					categoryList.get(lvl - 1).put(tmpStr[3].replaceAll("\"", "").trim(),tmpStr[4].replaceAll("\"", "").replaceAll("@", "").trim());
+				}
+			}
+			
+			// load 24h分類表(DMP_24h_category.csv)
+			Path category24HPath = Paths.get(path[3].toString());
+			List<String> lines24H = Files.readAllLines(category24HPath, charset);
+			for (String line : lines24H) {
+				CategoryCodeBean categoryBean = new CategoryCodeBean();
+				
+				String[] tmpStrAry = line.split(","); // 0001000000000000;M,35
+				
+				categoryBean.setNumberCode(tmpStrAry[0].replaceAll("\"", ""));
+				categoryBean.setChineseDesc(tmpStrAry[1].replaceAll("\"", ""));
+				categoryBean.setBreadCrumb(tmpStrAry[2].replaceAll("\"", ""));
+				categoryBean.setEnglishCode(tmpStrAry[3].replaceAll("\"", ""));
+				
+				category24hBeanList.add(categoryBean);
+			}
+			
+			// load Ruten分類表(DMP_Ruten_category.csv)
+			Path categoryRutenPath = Paths.get(path[4].toString());
+			List<String> linesRuten = Files.readAllLines(categoryRutenPath, charset);
+			for (String line : linesRuten) {
+				CategoryRutenCodeBean categoryRutenBean = new CategoryRutenCodeBean();
+				
+				String[] tmpStrAry = line.split(","); //"0001000000000000","電腦、電腦周邊"
+				
+				categoryRutenBean.setNumberCode(tmpStrAry[0].replaceAll("\"", ""));
+				categoryRutenBean.setChineseDesc(tmpStrAry[1].replaceAll("\"", ""));
+				
+				categoryRutenBeanList.add(categoryRutenBean);
+			}
+			
+			//IP轉城市
+			File database = new File(path[5].toString());
+			reader = new DatabaseReader.Builder(database).build();  
+			
+			
 //			//load 24h、ruten第3分類對照表(ThirdAdClassTable.txt)
 //			Path thirdAdClassPath = Paths.get(path[6].toString());
 //			charset = Charset.forName("UTF-8");
@@ -150,10 +150,10 @@ public class ThirdCategoryLogMapper extends Mapper<LongWritable, Text, Text, Tex
 //			for (String line : thirdAdClassLines) {
 //				prodFileList.add(line);
 //			}
-//			
-//		} catch (Exception e) {
-//			log.error("Mapper setup error>>>>>> " +e);
-//		}
+			
+		} catch (Exception e) {
+			log.error("Mapper setup error>>>>>> " +e);
+		}
 	}
 
 	@Override
@@ -222,15 +222,15 @@ public class ThirdCategoryLogMapper extends Mapper<LongWritable, Text, Text, Tex
 //	}
 	
 	
-//	public class combinedValue {
-//		public String gender;
-//		public String age;
-//
-//		public combinedValue(String gender, String age) {
-//			this.gender = gender;
-//			this.age = age;
-//		}
-//	}
+	public class combinedValue {
+		public String gender;
+		public String age;
+
+		public combinedValue(String gender, String age) {
+			this.gender = gender;
+			this.age = age;
+		}
+	}
 	
 
 //	 @Autowired
