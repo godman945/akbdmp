34a847ede65d18f0240cafe845bc2db50c578d04
alexchen <alexchen@alexchen6264.online.com.tw>
2018-11-30 11:44:54 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/pacllog/PaclLogConverCountReducer2.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/pacllog/PaclLogConverCountReducer2.java
@@ -1,26 +1,20 @@
 package com.pchome.hadoopdmp.mapreduce.job.pacllog;
 
-import java.sql.ResultSet;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
-import java.util.Map.Entry;
-import java.util.Set;
 
-import org.apache.commons.lang.StringUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 import com.pchome.soft.util.MysqlUtil;
@@ -31,50 +25,19 @@ import net.minidev.json.parser.JSONParser;
 @Component
 public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> {
 
-	private static Log log = LogFactory.getLog("DmpLogReducer");
-
+	private static Log log = LogFactory.getLog("PaclLogConverCountReducer2");
 	private Text keyOut = new Text();
-
 	private Text valueOut = new Text();
 
-	public static String record_date;
-
-	public static Producer<String, String> producer = null;
-
-	public RedisTemplate<String, Object> redisTemplate = null;
-
-	public int count;
-
-	public String redisFountKey;
-
-	public Map<String, JSONObject> kafkaDmpMap = null;
-
-	public Map<String, Integer> redisClassifyMap = null;
-	
-	public static Map<String, PcalConditionBean> convertConditionMap = new HashMap<String, PcalConditionBean>();
-
 	private MysqlUtil mysqlUtil = null;
-	
 	private StringBuffer convertWriteInfo = new StringBuffer();
-	
 	private StringBuffer sql = new StringBuffer();
-	
-	
+	private static Date date = new Date();
+	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+	private JSONParser jsonParser = new JSONParser(JSONParser.MODE_PERMISSIVE);
 	private static List<JSONObject> dataCkList = new ArrayList<JSONObject>();
 	private static List<JSONObject> dataPvList = new ArrayList<JSONObject>();
-	
 	private static Map<String,JSONObject> saveDBMap = new HashMap<String,JSONObject>();
-	
-	
-	private static Date date = new Date();
-	
-	private static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-	
-//	private JSONObject logJson = new JSONObject();
-	
-	
-	private JSONParser jsonParser = new JSONParser(JSONParser.MODE_PERMISSIVE);
-	
 	final static SimpleDateFormat sdfFormat =  new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 	private static boolean flagKdcl = false;
 	private static boolean flagPacl = false;
@@ -116,6 +79,7 @@ public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> 
 			iteratorJson = null;
 			iterator = null;
 			differenceDay = null;
+			convertSeq = "";
 			clickRangeDate = "";
 			impRangeDate = "";
 			convertPriceCount = "";
@@ -123,7 +87,6 @@ public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> 
 			kdclDate = "";
 			//1:最終 2:最初
 			convertBelong = "";
-			convertSeq = "";
 			flagKdcl = false;
 			flagPacl = false;
 			if(key.equals("2f59086e290c6a4a513834ba16f563e6")){
@@ -156,42 +119,47 @@ public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> 
 			}
 			if(flagKdcl && flagPacl){
 				log.info("##>>>>>>key:"+key);
-//				for (JSONObject json : dataCkList) {
-//					kdclDate = json.getAsString("kdclDate");
+				processOutOfRangeDay(dataCkList,"ck");
+				//排序時間
+				sortKdclDataList(dataCkList);
+				log.info("after sort kdclDate ck:"+ dataCkList);
+				//存入寫入DB map
+				processSaveDBInfo(dataCkList,"ck",key);
+				
+				
+				processOutOfRangeDay(dataPvList,"pv");
+				sortKdclDataList(dataPvList);
+				log.info("after sort kdclDate pv:"+ dataPvList);
+				processSaveDBInfo(dataPvList,"pv",key);
+				
+				
+				
+//				iterator = dataCkList.iterator();
+//				while (iterator.hasNext()) {
+//					iteratorJson = (JSONObject)iterator.next();
+//					kdclDate = iteratorJson.getAsString("kdclDate");
 //					differenceDay = (long) (date.getTime() - sdf.parse(kdclDate).getTime()) / (1000 * 60 * 60 *24);
 //					log.info("kdclDate ck:"+kdclDate+" flag:"+(differenceDay <= Long.valueOf(clickRangeDate)) + " range:"+differenceDay);
-//				}
-//				for (JSONObject json : dataCkList) {
-//					kdclDate = json.getAsString("kdclDate");
-//					differenceDay = (long) (date.getTime() - sdf.parse(kdclDate).getTime()) / (1000 * 60 * 60 *24);
 //					if(differenceDay > Long.valueOf(clickRangeDate)){
-//						dataCkList.remove(json);
+//						iterator.remove();
 //					}
 //				}
 				
 				
-				iterator = dataCkList.iterator();
-				while (iterator.hasNext()) {
-					iteratorJson = (JSONObject)iterator.next();
-					kdclDate = iteratorJson.getAsString("kdclDate");
-					differenceDay = (long) (date.getTime() - sdf.parse(kdclDate).getTime()) / (1000 * 60 * 60 *24);
-					log.info("kdclDate ck:"+kdclDate+" flag:"+(differenceDay <= Long.valueOf(clickRangeDate)) + " range:"+differenceDay);
-					if(differenceDay > Long.valueOf(clickRangeDate)){
-						iterator.remove();
-					}
-				}
-				
-				//排序時間
-				sortKdclDataList(dataCkList);
-				log.info("after sort kdclDate ck:"+ dataCkList);
-				if(dataCkList.size() > 0){
-					if(convertBelong.equals("1")){
-						log.info("final data:"+dataCkList.get(dataCkList.size() - 1));
-					}
-					if(convertBelong.equals("2")){
-						log.info("final data:"+dataCkList.get(0));
-					}
-				}
+//				if(dataCkList.size() > 0){
+//					if(convertBelong.equals("1")){
+//						JSONObject saveJson = new JSONObject();
+//						saveJson = dataCkList.get(dataCkList.size() - 1);
+//						saveDBMap.put(key+"_ck", saveJson);
+//						log.info("final data:"+dataCkList.get(dataCkList.size() - 1));
+//					}
+//					if(convertBelong.equals("2")){
+//						JSONObject saveJson = new JSONObject();
+//						saveJson = dataCkList.get(0);
+//						saveDBMap.put(key+"_ck", saveJson);
+//						log.info("final data:"+dataCkList.get(0));
+//					}
+//				}
 			}
 			
 		} catch (Throwable e) {
@@ -199,9 +167,45 @@ public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> 
 		}
 	}
 	
+	private void processSaveDBInfo(List<JSONObject> data,String type,String uuid) throws Exception{
+		if(data.size() > 0){
+			
+			if(convertBelong.equals("1")){
+				JSONObject saveJson = new JSONObject();
+				saveJson = data.get(data.size() - 1);
+				saveDBMap.put(uuid+"<PCHOME_"+type.toUpperCase()+">", saveJson);
+				log.info("final data:"+data.get(data.size() - 1));
+			}
+			if(convertBelong.equals("2")){
+				JSONObject saveJson = new JSONObject();
+				saveJson = data.get(0);
+				saveDBMap.put(uuid+"<PCHOME_"+type.toUpperCase()+">", saveJson);
+				log.info("final data:"+data.get(0));
+			}
+		}
+	}
 	
+	private void processOutOfRangeDay(List<JSONObject> data,String type) throws Exception{
+		log.info("processSaveDBInfo type:"+type);
+		String rangrDate = null;
+		if(type.equals("ck")){
+			rangrDate = clickRangeDate;
+		}else if(type.equals("pv")){
+			rangrDate = impRangeDate;
+		}
+		iterator = data.iterator();
+		while (iterator.hasNext()) {
+			iteratorJson = (JSONObject)iterator.next();
+			kdclDate = iteratorJson.getAsString("kdclDate");
+			differenceDay = (long) (date.getTime() - sdf.parse(kdclDate).getTime()) / (1000 * 60 * 60 *24);
+			log.info("kdclDate:"+kdclDate+" flag:"+(differenceDay <= Long.valueOf(rangrDate)) + " range:"+differenceDay);
+			if(differenceDay > Long.valueOf(rangrDate)){
+				iterator.remove();
+			}
+		}
+	}
 	
-	public List<JSONObject> sortKdclDataList(List<JSONObject> data){
+	private List<JSONObject> sortKdclDataList(List<JSONObject> data){
 		Collections.sort(dataCkList, new Comparator<JSONObject>() {
 			public int compare(JSONObject o1, JSONObject o2) {
 				try {
@@ -217,6 +221,10 @@ public class PaclLogConverCountReducer2 extends Reducer<Text, Text, Text, Text> 
 	
 	public void cleanup(Context context) {
 		try {
+			
+			log.info("cleanup:"+saveDBMap);
+			
+			
 			mysqlUtil.closeConnection();
 		} catch (Throwable e) {
 			convertWriteInfo.setLength(0);
