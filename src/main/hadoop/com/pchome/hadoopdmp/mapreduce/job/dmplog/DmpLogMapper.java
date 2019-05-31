67e03a0ccebb197ae67b8d9bc41adfd1141ce28d
alexchen <alexchen@alexchen6264.online.com.tw>
2019-05-31 17:31:33 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogMapper.java
@@ -177,6 +177,11 @@ public class DmpLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 			logStr = value.toString();
 			//1.判斷log來源為kdcl或bu
 			if(logpath.contains("alllog")) {
+				if(true) {
+					return;	
+				}
+				
+				
 				//kdcl log	raw data格式為一般或是Campaign
 				if(logStr.indexOf(kdclSymbol) > -1 ){
 					// values[0]  date time (2018-01-04 04:57:12)
