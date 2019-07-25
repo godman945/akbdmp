a6304cca18645f24eb5068183a45646ee2be8bf8
alexchen <alexchen@alexchen6264.online.com.tw>
2019-07-25 18:14:57 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
@@ -276,7 +276,7 @@ public class DmpLogReducer extends Reducer<Text, Text, Text, Text> {
 						wiriteToDruid.append("\""+dmpJSon.getAsString("fileName")+"\"");
 						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("log_date")).append("\"");
 						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("time_info_source")).append("\"");
-						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("memid")).append("\"");
+						wiriteToDruid.append(",").append("\"").append(dmpJSon.get("memid")).append("\"");
 						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("uuid")).append("\"");
 						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("uuid_flag")).append("\"");
 						wiriteToDruid.append(",").append("\"").append(dmpJSon.getAsString("ip")).append("\"");
