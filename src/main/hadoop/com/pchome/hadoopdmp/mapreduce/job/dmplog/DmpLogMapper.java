ac3f96bcfd9ad577030108ce0da0db881663d2e0
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-05 16:16:23 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogMapper.java
@@ -673,11 +673,11 @@ public class DmpLogMapper extends Mapper<LongWritable, Text, Text, Text> {
 //				keyOut.set(dmpDataJson.getAsString("uuid"));
 //				context.write(keyOut, new Text(dmpDataJson.toString()));
 				
-				for (int i= 0; i < markLevelList.length; i++) {
-					if(StringUtils.isNotBlank(dmpDataJson.getAsString(markLevelList[i]))) {
-						log.info(">>>>>>>>>>markLevelList:"+dmpDataJson.getAsString(markLevelList[i]));
+				for (int i= 0; i < markValueList.length; i++) {
+					if(StringUtils.isNotBlank(dmpDataJson.getAsString(markValueList[i]))) {
+						log.info(">>>>>>>>>>markValueList:"+dmpDataJson.getAsString(markValueList[i]));
 						
-						context.write(new Text(dmpDataJson.getAsString(markLevelList[i])), new Text(dmpDataJson.toString()));
+						context.write(new Text(dmpDataJson.getAsString(markValueList[i])), new Text(dmpDataJson.toString()));
 					}
 				}
 			} catch (Exception e) {
