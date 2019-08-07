e55a0ca654345242f2302e0259cc89355479057b
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-07 10:36:25 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
@@ -432,8 +432,22 @@ public class DmpLogReducer extends Reducer<Text, Text, Text, Text> {
 	    			detail.put("name_3", data4);
 	    			detail.put("code_3", data5);
 	    			total2 = total2 + 1;
+	    			
+	    			
+	    			
 	    			System.out.println(total2+">>>>>>>>>"+data5);
-	    			csvMap.put(data5, detail);
+	    			
+	    			if(csvMap.containsKey(data5)) {
+	    				System.out.println("NO************************************!:" + data5);
+	    			}else {
+	    				csvMap.put(data5, detail);	
+	    			}
+	    			
+	    			
+	    			
+	    			
+	    			
+	    			
 	    		}
 	    		reader.close();
 			}catch(Exception e) {
