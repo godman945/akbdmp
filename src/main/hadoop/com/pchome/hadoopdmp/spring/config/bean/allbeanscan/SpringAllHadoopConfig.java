7bdd04cac9b3615daba092aa31ce0429444824d3
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-14 17:26:26 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/spring/config/bean/allbeanscan/SpringAllHadoopConfig.java
+++ src/main/hadoop/com/pchome/hadoopdmp/spring/config/bean/allbeanscan/SpringAllHadoopConfig.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @ComponentScan(basePackages =  
 	"com.pchome.hadoopdmp.mapreduce.category,"
 	+"com.pchome.hadoopdmp.mapreduce.crawlbreadcrumb,"
-	+"com.pchome.akbdmp.spring.config.bean.property,"
+//	+"com.pchome.akbdmp.spring.config.bean.property,"
 	+"com.pchome.hadoop.category.job,"
 //	+"com.pchome.hadoopdmp.dao.sql,"
 	+"com.pchome.hadoopdmp.mapreduce.job.categorylog,"
