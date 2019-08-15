97acaa0aff8e38e1743b8989cd97756a5c687a0c
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-15 18:02:24 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
@@ -10,6 +10,8 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.reduce.IntSumReducer;
 
+import com.hadoop.mapreduce.LzoTextInputFormat;
+
 public class WordCount {
 	public static void main(String[] args) throws Exception {
 		Configuration conf = new Configuration();
@@ -18,16 +20,17 @@ public class WordCount {
 		FileSystem fs = FileSystem.get(conf);
 		System.out.println("1111111111");
 		System.out.println(fs.exists(new Path("/durid_source/word_count/a.txt")));
-		System.out.println(fs.exists(new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/word_count/a.txt")));
+		System.out.println(fs.exists(new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo")));
 		
 		Job job = new Job(conf, "word count");
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(TokenizerMapper.class);
 		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
+		job.setInputFormatClass(LzoTextInputFormat.class);
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
-		FileInputFormat.addInputPath(job, new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/word_count/a.txt"));
+		FileInputFormat.addInputPath(job, new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo"));
 		FileOutputFormat.setOutputPath(job, new Path("/durid_source"));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
 	}
