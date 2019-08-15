c34eb07bbfda78e490552b8eeaf7ef9c0a788c8e
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-15 09:55:32 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
@@ -46,8 +46,8 @@ public class DmpLogDriver {
 			
 			JobConf jobConf = new JobConf();
 			jobConf.setNumMapTasks(5);
-			jobConf.set("mapred.max.split.size","3045728"); //3045728 49 //3045728000 7
-			jobConf.set("mapred.min.split.size","1015544"); //1015544 49 //1015544000 7
+			jobConf.set("mapred.max.split.size","3045728000"); //3045728 49 //3045728000 7
+			jobConf.set("mapred.min.split.size","1015544000"); //1015544 49 //1015544000 7
 			//ask推测执行
 			jobConf.set("mapred.map.tasks.speculative.execution","true");
 			jobConf.set("mapred.reduce.tasks.speculative.execution","true");
