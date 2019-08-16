ec85c0276feab609f90b4b6ec8644e59c5e8c542
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-16 11:21:11 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
@@ -1,12 +1,18 @@
 package com.pchome.hadoopdmp.mapreduce.job.dmplog;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.filecache.DistributedCache;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.TextInputFormat;
+import org.apache.hadoop.io.compress.CompressionCodec;
+import org.apache.hadoop.io.compress.CompressionCodecFactory;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
@@ -22,8 +28,42 @@ public class WordCount {
 		FileSystem fs = FileSystem.get(conf);
 		fs.delete(new Path("hdfs://druid1.mypchome.com.tw:9000/durid_source"), true);
 		System.out.println("1111111111");
-		System.out.println(fs.exists(new Path("/durid_source/word_count/a.txt")));
 		System.out.println(fs.exists(new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo")));
+		
+		
+		
+		CompressionCodecFactory compressionCodecs = new CompressionCodecFactory(conf);
+    	System.out.println("11111111111111");
+		CompressionCodec codec = compressionCodecs.getCodec(new Path("/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo"));
+		
+		 InputStream in = null;
+		 OutputStream out = null;
+		 try {
+			 in = codec.createInputStream(fs.open(new Path("/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo")));
+			 
+			 
+			 StringBuilder stringBuilder = new StringBuilder();
+				String line = null;
+				
+				try (BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(in, "utf-8"))) {	
+					while ((line = bufferedReader.readLine()) != null) {
+						stringBuilder.append(line);
+						System.out.println(stringBuilder);
+					}
+				}
+		 } finally {
+			 in.close();
+		 }
+		System.out.println(codec == null);
+		
+		
+		
+		System.out.println("22222222222222222");
+		
+		
+		
+		
+		
 		
 		Job job = new Job(conf, "word count");
 		job.setJarByClass(WordCount.class);
