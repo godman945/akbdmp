b95ac20e28278801a881b6ec8531455dd24f3a94
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-16 09:45:27 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
@@ -19,6 +19,7 @@ public class WordCount {
 		
 		
 		FileSystem fs = FileSystem.get(conf);
+		fs.delete(new Path("hdfs://druid1.mypchome.com.tw:9000/durid_source"), true);
 		System.out.println("1111111111");
 		System.out.println(fs.exists(new Path("/durid_source/word_count/a.txt")));
 		System.out.println(fs.exists(new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo")));
@@ -26,11 +27,21 @@ public class WordCount {
 		Job job = new Job(conf, "word count");
 		job.setJarByClass(WordCount.class);
 		job.setMapperClass(TokenizerMapper.class);
-		job.setCombinerClass(IntSumReducer.class);
 		job.setReducerClass(IntSumReducer.class);
+		job.setCombinerClass(IntSumReducer.class);
+
+		
+		 job.setMapOutputKeyClass(Text.class);
+		 job.setMapOutputValueClass(IntWritable.class);
+		 job.setOutputKeyClass(Text.class);
+		 job.setOutputValueClass(IntWritable.class);
+		
+		
 //		job.setInputFormatClass(TextInputFormat.class);
-		job.setOutputKeyClass(Text.class);
-		job.setOutputValueClass(IntWritable.class);
+//		job.setMapOutputKeyClass(Text.class);
+//		job.setMapOutputValueClass(IntWritable.class);
+//		job.setOutputKeyClass(Text.class);
+//		job.setOutputValueClass(IntWritable.class);
 		FileInputFormat.addInputPath(job, new Path("hdfs://druid1.mypchome.com.tw:9000/druid_source/kdcl_log/2019-08-04/00/20190804_00_4c.log.lzo"));
 		FileOutputFormat.setOutputPath(job, new Path("/durid_source"));
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
