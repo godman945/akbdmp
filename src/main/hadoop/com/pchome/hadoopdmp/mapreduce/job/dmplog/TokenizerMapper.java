9193f690e1c75c4e7aff03b395ce91ef89ac7983
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-16 09:35:50 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/TokenizerMapper.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/TokenizerMapper.java
@@ -3,9 +3,11 @@ package com.pchome.hadoopdmp.mapreduce.job.dmplog;
 import java.io.IOException; 
 import java.util.StringTokenizer; 
  
-import org.apache.hadoop.io.IntWritable; 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text; 
-import org.apache.hadoop.mapreduce.Mapper; 
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context; 
  
 public class TokenizerMapper extends 
         Mapper<Object, Text, Text, IntWritable> { 
@@ -13,12 +15,16 @@ public class TokenizerMapper extends
     private final static IntWritable one = new IntWritable(1); 
     private Text word = new Text(); 
  
-    public void map(Object key, Text value, Context context) 
-            throws IOException, InterruptedException { 
-        StringTokenizer itr = new StringTokenizer(value.toString()); 
-        while (itr.hasMoreTokens()) { 
-            word.set(itr.nextToken()); 
-            context.write(word, one); 
-        } 
+    public synchronized void map(LongWritable offset, Text value, Context context) {
+    	
+    	System.out.println(value.toString());
+    	
+    	
+    	
+//        StringTokenizer itr = new StringTokenizer(value.toString()); 
+//        while (itr.hasMoreTokens()) { 
+//            word.set(itr.nextToken()); 
+//            context.write(word, one); 
+//        } 
     } 
 } 
