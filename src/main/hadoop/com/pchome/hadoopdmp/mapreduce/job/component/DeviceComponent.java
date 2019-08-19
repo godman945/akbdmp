965896ab28f131152c637eed37034ac7acb7ee35
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-19 10:43:45 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/DeviceComponent.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/DeviceComponent.java
@@ -1,6 +1,5 @@
 package com.pchome.hadoopdmp.mapreduce.job.component;
-
-import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 import eu.bitwalker.useragentutils.Browser;
