95f4d9714d4c521b65dd1b2f146c9c67bb1eee3b
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-19 10:53:47 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/DeviceComponent.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/DeviceComponent.java
@@ -12,31 +12,39 @@ public class DeviceComponent {
 	private static Browser browser  = null;
 	private static OperatingSystem operatingSystem = null;
 	public net.minidev.json.JSONObject parseUserAgentToDevice(net.minidev.json.JSONObject dmpJSon) throws Exception {
-		if (StringUtils.isBlank(dmpJSon.getAsString("user_agent"))){
-			dmpJSon.put("device_info_classify", "");
-			return dmpJSon;
-		}else {
-			userAgent = null;
-			browser  = null;
-			operatingSystem = null;
-			userAgent = UserAgent.parseUserAgentString(dmpJSon.getAsString("user_agent"));
-			browser = userAgent.getBrowser();
-			operatingSystem = userAgent.getOperatingSystem();
-			
-			dmpJSon.put("device_info", operatingSystem.getDeviceType().toString());
-			dmpJSon.put("device_phone_info", operatingSystem.getManufacturer().toString());
-			dmpJSon.put("device_os_info", operatingSystem.getGroup().toString());
-			dmpJSon.put("device_browser_info", browser.getGroup().toString());
-			dmpJSon.put("device_info_source", "user-agent");
-			if ((!StringUtils.equals(dmpJSon.getAsString("device_info"),"UNKNOWN")) && 
-			   (!StringUtils.equals(dmpJSon.getAsString("device_phone_info"),"UNKNOWN")) &&
-			   (!StringUtils.equals(dmpJSon.getAsString("device_os_info"),"UNKNOWN")) && 
-			   (!StringUtils.equals(dmpJSon.getAsString("device_browser_info"),"UNKNOWN"))){
-					dmpJSon.put("device_info_classify", "Y");
-				}else{
-					dmpJSon.put("device_info_classify", "N");
-				}
-			return dmpJSon;
-		}
+		
+		System.out.println(dmpJSon);
+		net.minidev.json.JSONObject n = new net.minidev.json.JSONObject();
+		n.put("alex", "XXXXX");
+		System.out.println(n.getAsString("alex"));
+		System.out.println("-----");
+		
+//		if (StringUtils.isBlank(dmpJSon.getAsString("user_agent"))){
+//			dmpJSon.put("device_info_classify", "");
+//			return dmpJSon;
+//		}else {
+//			userAgent = null;
+//			browser  = null;
+//			operatingSystem = null;
+//			userAgent = UserAgent.parseUserAgentString(dmpJSon.getAsString("user_agent"));
+//			browser = userAgent.getBrowser();
+//			operatingSystem = userAgent.getOperatingSystem();
+//			
+//			dmpJSon.put("device_info", operatingSystem.getDeviceType().toString());
+//			dmpJSon.put("device_phone_info", operatingSystem.getManufacturer().toString());
+//			dmpJSon.put("device_os_info", operatingSystem.getGroup().toString());
+//			dmpJSon.put("device_browser_info", browser.getGroup().toString());
+//			dmpJSon.put("device_info_source", "user-agent");
+//			if ((!StringUtils.equals(dmpJSon.getAsString("device_info"),"UNKNOWN")) && 
+//			   (!StringUtils.equals(dmpJSon.getAsString("device_phone_info"),"UNKNOWN")) &&
+//			   (!StringUtils.equals(dmpJSon.getAsString("device_os_info"),"UNKNOWN")) && 
+//			   (!StringUtils.equals(dmpJSon.getAsString("device_browser_info"),"UNKNOWN"))){
+//					dmpJSon.put("device_info_classify", "Y");
+//				}else{
+//					dmpJSon.put("device_info_classify", "N");
+//				}
+//			return dmpJSon;
+//		}
+		return dmpJSon;
 	}
 }
