4dd2e990211d17cdfa25dfe1699ff1e8c0a6adab
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-19 10:31:44 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/GeoIpComponent.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/component/GeoIpComponent.java
@@ -1,15 +1,11 @@
 package com.pchome.hadoopdmp.mapreduce.job.component;
 
 import java.net.InetAddress;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import com.maxmind.geoip2.model.CityResponse;
 import com.pchome.hadoopdmp.mapreduce.job.component.IpAddress.IpAdd;
 import com.pchome.hadoopdmp.mapreduce.job.dmplog.DmpLogMapper;
-
-import net.minidev.json.JSONObject;
 
 public class GeoIpComponent {
 
@@ -17,28 +13,13 @@ public class GeoIpComponent {
 	private static String ip = ""; 
 	private static IpAdd ipAdd = new IpAdd();
 	private static CityResponse response = null;
-	public net.minidev.json.JSONObject ipTransformGEO(net.minidev.json.JSONObject dmpJSon_997) throws Exception {
-		net.minidev.json.JSONObject dmpJSon = (net.minidev.json.JSONObject)dmpJSon_997;
-		
-		System.out.println("11111111111----:"+dmpJSon_997);
+	public net.minidev.json.JSONObject ipTransformGEO(net.minidev.json.JSONObject dmpJSon) throws Exception {
 		// 判斷是否為正確ip格式
-		System.out.println("ZZZZZZZZZZZZZz000000");
-		System.out.println(dmpJSon_997.get("trigger_type"));
-		System.out.println(dmpJSon_997.get("ip"));
-		System.out.println(ipAdd.isIP(dmpJSon_997.get("ip").toString()));
-		
-		
-		System.out.println("QQQQQQQQQQQQQQQQ:"+((net.minidev.json.JSONObject)dmpJSon_997).getAsString("ip"));
-		
-		ip = dmpJSon_997.get("ip").toString();
-		
-		System.out.println("ZZZZZZZZZZZZZz");
-		if (!ipAdd.isIP(dmpJSon_997.get("ip").toString())) {
-			System.out.println("area_info_classify ><><><>< null");
-			dmpJSon_997.put("area_info_classify", "");
-			return dmpJSon_997;
+		ip = dmpJSon.get("ip").toString();
+		if (!ipAdd.isIP(ip)) {
+			dmpJSon.put("area_info_classify", "");
+			return dmpJSon;
 		}
-		System.out.println("2222222222222");
 		// ip轉換國家、城市
 		InetAddress ipAddress = DmpLogMapper.ipAddress.getByName(ip);
 		try {
@@ -47,16 +28,14 @@ public class GeoIpComponent {
 			System.out.println("The address is not in the database:" + ip);
 			throw e;
 		}
-		System.out.println("33333333333");
 		if (response == null) {
-			dmpJSon_997.put("area_info_classify", "N");
-			return dmpJSon_997;
+			dmpJSon.put("area_info_classify", "N");
+			return dmpJSon;
 		}
 		String countryStr = response.getCountry().getName();
 		String cityStr = response.getCity().getNames().get("en");
-		dmpJSon_997.put("area_country", countryStr);
-		dmpJSon_997.put("area_city", cityStr);
-		System.out.println("44444444444");
-		return dmpJSon_997;
+		dmpJSon.put("area_country", countryStr);
+		dmpJSon.put("area_city", cityStr);
+		return dmpJSon;
 	}
 }
