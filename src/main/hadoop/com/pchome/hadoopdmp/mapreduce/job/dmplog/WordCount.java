15c09fe5e77495559905e1ec72e899a5d6b0ad3b
alexchen <alexchen@alexchen6264.online.com.tw>
2019-08-21 12:07:46 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/WordCount.java
@@ -28,8 +28,8 @@ public class WordCount {
 		System.setProperty("hadoop.home.dir", "D:\\hadoop_2.8.5");
 		System.getProperties().put("HADOOP_USER_NAME", "webuser");
 		Configuration conf = new Configuration();
-		conf.set("mapreduce.map.output.compress.codec", "com.hadoop.mapreduce.LzoTextInputFormat");
-		conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+//		conf.set("mapreduce.map.output.compress.codec", "com.hadoop.mapreduce.LzoTextInputFormat");
+//		conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
 		conf.set("io.compression.codecs", "org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzoCodec,com.hadoop.compression.lzo.LzopCodec,org.apache.hadoop.io.compress.BZip2Codec");
 		conf.set("io.compression.codec.lzo.class", "com.hadoop.compression.lzo.LzoCodec");
 		conf.set("mapred.compress.map.output", "true");
