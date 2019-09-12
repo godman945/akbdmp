1dff8ffac10c500e6def5ab52945abc1796a125c
alexchen <alexchen@alexchen6264.online.com.tw>
2019-09-12 11:54:25 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogReducer.java
@@ -206,6 +206,7 @@ public class DmpLogReducer extends Reducer<Text, Text, Text, Text> {
 				
 				Collections.sort(logJsonList, new Comparator<JSONObject>() {
 					public int compare(JSONObject a, JSONObject b) {
+						SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 					    try {
 					    	System.out.println("FFFFF");
 							return sdf.parse(b.getAsString("log_date")).compareTo(sdf.parse(a.getAsString("log_date")));
