34b3a478480f466b30b8acb8fe5ba98d780fd177
alexchen <alexchen@alexchen6264.online.com.tw>
2019-12-31 13:39:21 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
@@ -83,13 +83,13 @@ public class DmpLogDriver {
 			
 			// hdfs
 			Configuration conf = new Configuration();
-			conf.set("mapreduce.map.output.compress.codec", "com.hadoop.mapreduce.LzoTextInputFormat");
-			conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
-			conf.set("io.compression.codecs", "org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzoCodec,com.hadoop.compression.lzo.LzopCodec,org.apache.hadoop.io.compress.BZip2Codec");
-			conf.set("io.compression.codec.lzo.class", "com.hadoop.compression.lzo.LzoCodec");
-			conf.set("mapred.compress.map.output", "true");
-			conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
-	        conf.set("mapred.child.java.opts", "-Xmx4048M");
+//			conf.set("mapreduce.map.output.compress.codec", "com.hadoop.mapreduce.LzoTextInputFormat");
+//			conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+//			conf.set("io.compression.codecs", "org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzoCodec,com.hadoop.compression.lzo.LzopCodec,org.apache.hadoop.io.compress.BZip2Codec");
+//			conf.set("io.compression.codec.lzo.class", "com.hadoop.compression.lzo.LzoCodec");
+//			conf.set("mapred.compress.map.output", "true");
+//			conf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
+//	        conf.set("mapred.child.java.opts", "-Xmx4048M");
 			
 			
 			
@@ -142,14 +142,12 @@ public class DmpLogDriver {
 	        	
 	  			
 	  			Path bupath =   new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/bu_log/"+dmpDate+"/bu_"+dmpDate+"_log.lzo");
-	  			Path bupath2 =   new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/bu_log/2019-11-01/bu_2019-11-01_log.lzo.index");
 	  			
 //	  			Path kdclpath = new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/kdcl_log/"+dmpDate+"/kdcl_"+dmpDate+"_log.lzo");
 //	  			Path paclpath = new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/pacl_log/"+dmpDate+"/pacl_"+dmpDate+"_log.lzo");
 	  			
 	  			
 	  			listPath.add(bupath);
-	  			listPath.add(bupath2);
 //  				listPath.add(kdclpath);
 //  				listPath.add(paclpath);
 	  			
