bdbb9ee3a32b25901dde64ab5386ce643a796bc2
alexchen <alexchen@alexchen6264.online.com.tw>
2019-12-31 11:35:02 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
@@ -74,9 +74,9 @@ public class DmpLogDriver {
 			jobConf.set("job.date",dmpDate);
 			jobConf.set("job.hour",dmpHour);
 			
-			
+			jobConf.set("io.compression.codecs", "org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzopCodec");
 //			jobConf.set("io.compression.codecs", "org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,com.hadoop.compression.lzo.LzoCodec,com.hadoop.compression.lzo.LzopCodec,org.apache.hadoop.io.compress.BZip2Codec");
-			jobConf.set("io.compression.codec.lzo.class", "com.hadoop.compression.lzo.LzoCodec");
+//			jobConf.set("io.compression.codec.lzo.class", "com.hadoop.compression.lzo.LzoCodec");
 //			jobConf.set("mapred.compress.map.output", "true");
 //			jobConf.set("mapred.map.output.compression.codec", "com.hadoop.compression.lzo.LzoCodec");
 			
