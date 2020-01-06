cf637363d7814b58a9ef8fe7fa7ed38e9d8bf28d
alexchen <alexchen@alexchen6264.online.com.tw>
2020-01-06 14:11:02 +0800

    update

--- src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
+++ src/main/hadoop/com/pchome/hadoopdmp/mapreduce/job/dmplog/DmpLogDriver.java
@@ -155,9 +155,9 @@ public class DmpLogDriver {
 //				}
 	        	
 	        	
-	        	Path bupath =   new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/bu_log/"+dmpDate+"/"+dmpHour+"/bu_"+dmpDate+"_log.lzo");
+	        	Path bupath =   new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/bu_log/"+dmpDate+"/"+dmpHour+"/bu_"+dmpDate+"_"+dmpHour+"_log.lzo");
 	  			Path kdclpath = new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/kdcl_log/"+dmpDate+"/"+dmpHour+"/kdcl_"+dmpDate+"_"+dmpHour+"_log.lzo");
-	  			Path paclpath = new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/pacl_log/"+dmpDate+"/"+dmpHour+"/pacl_"+dmpDate+"_log.lzo");
+	  			Path paclpath = new Path("hdfs://hdn1.mypchome.com.tw:9000/druid/dmp_log_source/pacl_log/"+dmpDate+"/"+dmpHour+"/pacl_"+dmpDate+"_"+dmpHour+"_log.lzo");
 	  			
 	  			
 	  			listPath.add(bupath);
