7ce8b9c4b80a4be6c18f12287cb5c80091b9bc7f
User <User@LAPTOP-GE5OK6A4>
2020-11-05 08:48:02 +0800

    TEST

--- src/main/hadoop/com/pchome/hadoopdmp/dao/IClassCountDAO.java
+++ src/main/hadoop/com/pchome/hadoopdmp/dao/IClassCountDAO.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.mongodb.DBObject;
 
 public interface IClassCountDAO {
-
+	
 	public int insert(List<DBObject> list) throws Exception;
 
     public int insert(DBObject dbObject) throws Exception;
